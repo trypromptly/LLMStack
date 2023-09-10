5bf5863ad6947941f1def073ab0e46fcea82c5e8
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-09-10 13:35:28 -0700

    Replace antd with MUI in AppStudioPage

--- client/src/pages/AppStudio.jsx
+++ client/src/pages/AppStudio.jsx
@@ -1,5 +1,5 @@
-import { Typography } from "@mui/material";
-import { Col, Divider, Row } from "antd";
+import React from "react";
+import { Typography, Container, Divider } from "@mui/material";
 import { AppTypeSelector } from "../components/apps/AppTypeSelector";
 import { AppList } from "../components/apps/AppList";
 import { AppTemplatesList } from "../components/apps/AppTemplatesList";
@@ -7,101 +7,89 @@ import { SharedAppList } from "../components/apps/SharedAppList";
 
 const AppStudioPage = () => {
   return (
-    <div id="app-studio-page" style={{ margin: 10 }}>
-      <Col span={24}>
-        <Row>
-          <Typography
-            style={{
-              textAlign: "left",
-              width: "100%",
-              fontFamily: "Lato, sans-serif",
-              marginBottom: "10px",
-              padding: "5px 10px",
-              fontWeight: 600,
-              borderRadius: "5px",
-              color: "#1c3c5a",
-              fontSize: "18px",
-              borderBottom: "solid 3px #1c3c5a",
-              borderLeft: "solid 1px #ccc",
-              borderRight: "solid 1px #ccc",
-            }}
-            variant="h6"
-          >
-            Quickstart App Templates
-          </Typography>
-          <AppTemplatesList />
-        </Row>
-        <Divider />
-        <Row>
-          <Typography
-            style={{
-              textAlign: "left",
-              width: "100%",
-              fontFamily: "Lato, sans-serif",
-              marginBottom: "10px",
-              padding: "5px 10px",
-              fontWeight: 600,
-              borderRadius: "5px",
-              color: "#1c3c5a",
-              fontSize: "18px",
-              borderBottom: "solid 3px #1c3c5a",
-              borderLeft: "solid 1px #ccc",
-              borderRight: "solid 1px #ccc",
-            }}
-            variant="h6"
-          >
-            Create a new App from scratch
-          </Typography>
-          <AppTypeSelector />
-        </Row>
-        <Divider />
-        <Row>
-          <Typography
-            style={{
-              textAlign: "left",
-              width: "100%",
-              fontFamily: "Lato, sans-serif",
-              marginBottom: "10px",
-              padding: "5px 10px",
-              fontWeight: 600,
-              borderRadius: "5px",
-              color: "#1c3c5a",
-              fontSize: "18px",
-              borderBottom: "solid 3px #1c3c5a",
-              borderLeft: "solid 1px #ccc",
-              borderRight: "solid 1px #ccc",
-            }}
-            variant="h6"
-          >
-            Your Apps
-          </Typography>
-          <AppList />
-        </Row>
-        <Divider />
-        <Row>
-          <Typography
-            style={{
-              textAlign: "left",
-              width: "100%",
-              fontFamily: "Lato, sans-serif",
-              marginBottom: "10px",
-              padding: "5px 10px",
-              fontWeight: 600,
-              borderRadius: "5px",
-              color: "#1c3c5a",
-              fontSize: "18px",
-              borderBottom: "solid 3px #1c3c5a",
-              borderLeft: "solid 1px #ccc",
-              borderRight: "solid 1px #ccc",
-            }}
-            variant="h6"
-          >
-            Apps Shared With You
-          </Typography>
-          <SharedAppList />
-        </Row>
-      </Col>
-    </div>
+    <Container maxWidth="md" style={{ minWidth: "100%" }}>
+      <Typography
+        variant="h6"
+        style={{
+          textAlign: "left",
+          marginBottom: "10px",
+          padding: "5px 10px",
+          fontWeight: 600,
+          borderRadius: "5px",
+          color: "#1c3c5a",
+          fontSize: "18px",
+          borderBottom: "solid 3px #1c3c5a",
+          borderLeft: "solid 1px #ccc",
+          borderRight: "solid 1px #ccc",
+        }}
+      >
+        Quickstart App Templates
+      </Typography>
+      <AppTemplatesList />
+
+      <Divider />
+
+      <Typography
+        variant="h6"
+        style={{
+          textAlign: "left",
+          marginBottom: "10px",
+          padding: "5px 10px",
+          fontWeight: 600,
+          borderRadius: "5px",
+          color: "#1c3c5a",
+          fontSize: "18px",
+          borderBottom: "solid 3px #1c3c5a",
+          borderLeft: "solid 1px #ccc",
+          borderRight: "solid 1px #ccc",
+        }}
+      >
+        Create a new App from scratch
+      </Typography>
+      <AppTypeSelector />
+
+      <Divider />
+
+      <Typography
+        variant="h6"
+        style={{
+          textAlign: "left",
+          marginBottom: "10px",
+          padding: "5px 10px",
+          fontWeight: 600,
+          borderRadius: "5px",
+          color: "#1c3c5a",
+          fontSize: "18px",
+          borderBottom: "solid 3px #1c3c5a",
+          borderLeft: "solid 1px #ccc",
+          borderRight: "solid 1px #ccc",
+        }}
+      >
+        Your Apps
+      </Typography>
+      <AppList />
+
+      <Divider />
+
+      <Typography
+        variant="h6"
+        style={{
+          textAlign: "left",
+          marginBottom: "10px",
+          padding: "5px 10px",
+          fontWeight: 600,
+          borderRadius: "5px",
+          color: "#1c3c5a",
+          fontSize: "18px",
+          borderBottom: "solid 3px #1c3c5a",
+          borderLeft: "solid 1px #ccc",
+          borderRight: "solid 1px #ccc",
+        }}
+      >
+        Apps Shared With You
+      </Typography>
+      <SharedAppList />
+    </Container>
   );
 };
 
