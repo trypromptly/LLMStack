58f171a97d74d384d9ceb84b6e3d2fefaed47403
Ajay Chintala <nitk.ajay@gmail.com>
2023-09-14 13:04:46 -0700

    Allow job to run locally

--- jobs/adhoc.py
+++ jobs/adhoc.py
@@ -1,17 +1,26 @@
 import uuid
-
 import django_rq
+
+from django.conf import settings
 from rq.job import Job
+from threading import Thread
+from queue import Queue
 
 
 class ProcessingJob(Job):
+    _use_redis = settings.USE_REMOTE_JOB_QUEUE if hasattr(
+        settings, 'USE_REMOTE_JOB_QUEUE') else True
+
     @property
     def queue_name(self):
         return 'default'
 
     @property
-    def job_queue(cls):
-        return django_rq.queues.get_queue(cls.queue_name)
+    def job_queue(self):
+        if self._use_redis:
+            return django_rq.queues.get_queue(self.queue_name)
+        else:
+            return Queue()
 
     @classmethod
     def generate_job_id(self):
@@ -19,15 +28,24 @@ class ProcessingJob(Job):
 
     @classmethod
     def get_connection(self):
-        return django_rq.get_connection('default')
+        if self._use_redis:
+            return django_rq.get_connection('default')
+        else:
+            return 'local'  # Return a dummy connection
 
     @classmethod
     def create(cls, **kwargs):
         return super().create(id=cls.generate_job_id(), connection=cls.get_connection(), **kwargs)
 
     def add_to_queue(self, *args, **kwargs) -> Job:
-        queue = django_rq.get_queue(self.queue_name)
-        return queue.enqueue_job(job=self)
+        if self._use_redis:
+            queue = django_rq.get_queue(self.queue_name)
+            return queue.enqueue_job(job=self)
+        else:
+            thread = Thread(target=self.func, args=self.args,
+                            kwargs=self.kwargs)
+            thread.start()
+            return thread
 
 
 class DataSourceEntryProcessingJob(ProcessingJob):
