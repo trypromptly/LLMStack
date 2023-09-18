10efb6380fded19f39f403f74d06cc61a7c1e1e9
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-09-18 00:34:50 -0700

    Send errors back to slack and discord (#40)

--- processors/providers/discord/send_message.py
+++ processors/providers/discord/send_message.py
@@ -1,11 +1,10 @@
 import logging
 import time
-from typing import Dict
+from typing import Any
 from typing import List
 from typing import Optional
 
 from asgiref.sync import async_to_sync
-from pydantic import Field
 
 from common.blocks.http import BearerTokenAuth
 from common.blocks.http import HttpAPIProcessor
@@ -67,31 +66,48 @@ class DiscordSendMessageProcessor(
     def provider_slug() -> str:
         return 'discord'
 
-    def process(self) -> dict:
-        _env = self._env
-        input = self._input.dict()
-        url = f"https://discord.com/api/v10/webhooks/{input['app_id']}/{input['token']}"
-
+    def _send_message(self, app_id: str, message: str, token: str) -> None:
+        url = f"https://discord.com/api/v10/webhooks/{app_id}/{token}"
         http_processor = HttpAPIProcessor(configuration={'timeout': 60})
         response = http_processor.process(
             HttpAPIProcessorInput(
                 url=url,
                 method=HttpMethod.POST,
                 headers={'Content-Type': 'application/json'},
-                authorization=BearerTokenAuth(token=input['token']),
+                authorization=BearerTokenAuth(token=token),
                 body=JsonBody(
                     json_body={
-                        'content': input['text'],
+                        'content': message,
                     },
                 ),
             ).dict(),
         )
+        return response
+        
+    def process(self) -> dict:
+        _env = self._env
+        input = self._input.dict()
+        response = self._send_message(input['app_id'], input['text'], input['token'])
 
         async_to_sync(self._output_stream.write)(
             DiscordSendMessageOutput(code=200),
         )
 
         return self._output_stream.finalize()
+    
+    def on_error(self, error: Any) -> None:
+        input = self._input.dict()
+        logger.error(f'Error in DiscordSendMessageProcessor: {error}')
+        error_msg = '\n'.join(error.values()) if isinstance(error, dict) else 'Error in processing request'
+        
+        self._send_message(input['app_id'], error_msg, input['token'])
+        
+        async_to_sync(self._output_stream.write)(
+            DiscordSendMessageOutput(code=200),
+        )
+        self._output_stream.finalize()
+        
+        return super().on_error(error)
 
     def get_bookkeeping_data(self) -> BookKeepingData:
         return BookKeepingData(input=self._input, timestamp=time.time(), run_data={'discord': {'user': self._input.discord_user_id, 'username': self._input.discord_username, 'global_name': self._input.discord_global_name}})
