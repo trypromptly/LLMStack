875d16cfb45725ad3d3893010411443d20892078
Ajay Chintala <nitk.ajay@gmail.com>
2023-09-26 10:48:37 -0700

    Remove subdomain routing

--- llmstack/client/src/index.js
+++ llmstack/client/src/index.js
@@ -33,263 +33,242 @@ let router = null;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-if (
-  window.location.host.split(".").length === 4 &&
-  window.location.host.split(".")[1] === "app"
-) {
-  let subdomain = window.location.host.split(".")[1];
-  if (subdomain === "app") {
-    const publishedAppIdParam = window.location.host.split(".")[0];
-    router = createBrowserRouter([
-      {
-        path: "/",
-        element: (
-          <AppRenderPage
-            headless={true}
-            publishedAppIdParam={publishedAppIdParam}
-          />
-        ),
-        errorElement: <ErrorPage />,
-      },
-    ]);
-  }
-} else {
-  router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <App>
-          <AppStudioPage />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/playground",
-      element: (
-        <App>
-          <HomePage isSharedPageMode={false} />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps",
-      element: (
-        <App>
-          <AppStudioPage />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/templates/:appTemplateSlug",
-      element: (
-        <App>
-          <AppStudioPage />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appTypeSlug/create",
-      element: (
-        <App>
-          <AppEditPage />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId",
-      element: (
-        <App>
-          <AppEditPage />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/preview",
-      element: (
-        <App>
-          <AppEditPage page="preview" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/template",
-      element: (
-        <App>
-          <AppEditPage page="template" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/editor",
-      element: (
-        <App>
-          <AppEditPage page="editor" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/editor",
-      element: (
-        <App>
-          <AppEditPage page="editor" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/history",
-      element: (
-        <App>
-          <AppEditPage page="history" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/tests",
-      element: (
-        <App>
-          <AppEditPage page="tests" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/versions",
-      element: (
-        <App>
-          <AppEditPage page="versions" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/integrations/website",
-      element: (
-        <App>
-          <AppEditPage page="integrations/website" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/integrations/api",
-      element: (
-        <App>
-          <AppEditPage page="integrations/api" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/integrations/slack",
-      element: (
-        <App>
-          <AppEditPage page="integrations/slack" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/integrations/discord",
-      element: (
-        <App>
-          <AppEditPage page="integrations/discord" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/apps/:appId/preview",
-      element: (
-        <App>
-          <AppEditPage page="preview" />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/app/:publishedAppId/:embed?/:chatBubble?",
-      element: <AppRenderPage headless={true} />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/hub",
-      element: (
-        <App>
-          <Discover />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/s/:shareId",
-      element: (
-        <App>
-          <HomePage isSharedPageMode={true} />
-        </App>
-      ),
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/dashboard",
-      element: (
-        <App>
-          <DashboardPage />
-        </App>
-      ),
-    },
-    {
-      path: "/history",
-      element: (
-        <App>
-          <HistoryPage />
-        </App>
-      ),
-    },
-    {
-      path: "/settings",
-      element: (
-        <App>
-          <SettingPage />
-        </App>
-      ),
-    },
-    {
-      path: "/organization",
-      element: (
-        <App>
-          <OrganizationPage />
-        </App>
-      ),
-    },
-    {
-      path: "/data",
-      element: (
-        <App>
-          <DataPage />
-        </App>
-      ),
-    },
-    {
-      path: "/login",
-      element: <LoginPage />,
-    },
-    {
-      path: "/signup",
-      element: <SignupPage />,
-    },
-  ]);
-}
+router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App>
+        <AppStudioPage />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/playground",
+    element: (
+      <App>
+        <HomePage isSharedPageMode={false} />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps",
+    element: (
+      <App>
+        <AppStudioPage />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/templates/:appTemplateSlug",
+    element: (
+      <App>
+        <AppStudioPage />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appTypeSlug/create",
+    element: (
+      <App>
+        <AppEditPage />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId",
+    element: (
+      <App>
+        <AppEditPage />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/preview",
+    element: (
+      <App>
+        <AppEditPage page="preview" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/template",
+    element: (
+      <App>
+        <AppEditPage page="template" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/editor",
+    element: (
+      <App>
+        <AppEditPage page="editor" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/editor",
+    element: (
+      <App>
+        <AppEditPage page="editor" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/history",
+    element: (
+      <App>
+        <AppEditPage page="history" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/tests",
+    element: (
+      <App>
+        <AppEditPage page="tests" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/versions",
+    element: (
+      <App>
+        <AppEditPage page="versions" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/integrations/website",
+    element: (
+      <App>
+        <AppEditPage page="integrations/website" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/integrations/api",
+    element: (
+      <App>
+        <AppEditPage page="integrations/api" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/integrations/slack",
+    element: (
+      <App>
+        <AppEditPage page="integrations/slack" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/integrations/discord",
+    element: (
+      <App>
+        <AppEditPage page="integrations/discord" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/apps/:appId/preview",
+    element: (
+      <App>
+        <AppEditPage page="preview" />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/app/:publishedAppId/:embed?/:chatBubble?",
+    element: <AppRenderPage headless={true} />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/hub",
+    element: (
+      <App>
+        <Discover />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/s/:shareId",
+    element: (
+      <App>
+        <HomePage isSharedPageMode={true} />
+      </App>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <App>
+        <DashboardPage />
+      </App>
+    ),
+  },
+  {
+    path: "/history",
+    element: (
+      <App>
+        <HistoryPage />
+      </App>
+    ),
+  },
+  {
+    path: "/settings",
+    element: (
+      <App>
+        <SettingPage />
+      </App>
+    ),
+  },
+  {
+    path: "/organization",
+    element: (
+      <App>
+        <OrganizationPage />
+      </App>
+    ),
+  },
+  {
+    path: "/data",
+    element: (
+      <App>
+        <DataPage />
+      </App>
+    ),
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
