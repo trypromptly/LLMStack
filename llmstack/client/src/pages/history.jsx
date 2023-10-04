85690adf02b73716dab80baee75ad70b1ec383c1
Ajay Chintala <nitk.ajay@gmail.com>
2023-10-04 09:52:40 -0700

    Add bottom margin to history

--- llmstack/client/src/pages/history.jsx
+++ llmstack/client/src/pages/history.jsx
@@ -2,7 +2,7 @@ import { AppRunHistoryTimeline } from "../components/apps/AppRunHistoryTimeline"
 
 export default function HistoryPage() {
   return (
-    <div id="history-page">
+    <div id="history-page" style={{ marginBottom: "60px" }}>
       <AppRunHistoryTimeline showFilterBar={true} />
     </div>
   );
