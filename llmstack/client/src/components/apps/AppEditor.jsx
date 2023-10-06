1ec99126ae16308cc260762a3c97eea8d073d47d
Ajay Chintala <nitk.ajay@gmail.com>
2023-10-06 12:44:40 -0700

    Add YAML editor for app config

--- llmstack/client/src/components/apps/AppEditor.jsx
+++ llmstack/client/src/components/apps/AppEditor.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
-import { Box, Stack } from "@mui/material";
+import {
+  Box,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Tooltip,
+} from "@mui/material";
+import yaml from "js-yaml";
+import AceEditor from "react-ace";
 import { ProcessorEditor } from "./ProcessorEditor";
 import { AppConfigEditor } from "./AppConfigEditor";
 import { AppOutputEditor } from "./AppOutputEditor";
 import { AddProcessorDivider } from "./AddProcessorDivider";
 import { getJSONSchemaFromInputFields } from "../../data/utils";
 import { AppSaveButtons } from "./AppSaveButtons";
+import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/theme-dracula";
 
 export function AppEditor(props) {
   const {
     app,
+    setApp,
     saveApp,
     processors,
     setProcessors,
@@ -22,6 +33,7 @@ export function AppEditor(props) {
   } = props;
   const [activeStep, setActiveStep] = useState(1);
   const [outputSchemas, setOutputSchemas] = useState([]);
+  const [editorType, setEditorType] = useState("ui");
 
   useEffect(() => {
     const { schema } = getJSONSchemaFromInputFields(appInputFields);
@@ -45,73 +57,124 @@ export function AppEditor(props) {
 
   return (
     <Box>
-      <Stack>
-        <AppConfigEditor
-          appType={app?.type}
-          activeStep={activeStep}
-          setActiveStep={setActiveStep}
-          config={appConfig}
-          setConfig={setAppConfig}
-          inputFields={appInputFields}
-          setInputFields={setAppInputFields}
-          processors={processors}
-          setProcessors={setProcessors}
-          outputSchemas={outputSchemas}
-        />
-      </Stack>
-      {processors.map((processor, index) => (
-        <Stack style={{ justifyContent: "center" }} key={index}>
-          <AddProcessorDivider showProcessorSelector={false} />
-          <ProcessorEditor
-            appId={app.uuid}
-            index={index}
-            processors={processors}
-            setProcessors={setProcessors}
-            activeStep={activeStep}
-            setActiveStep={setActiveStep}
-            outputSchemas={outputSchemas}
-            isTool={app.app_type_slug === "agent"}
+      {editorType === "yaml" ? (
+        <Box>
+          <AceEditor
+            mode="yaml"
+            theme="dracula"
+            value={yaml.dump(app?.data || {})}
+            editorProps={{ $blockScrolling: true }}
+            setOptions={{
+              useWorker: false,
+              showGutter: false,
+            }}
+            style={{
+              borderRadius: "5px",
+              width: "100%",
+            }}
+            onLoad={(editor) => {
+              editor.renderer.setScrollMargin(10, 0, 10, 0);
+              editor.renderer.setPadding(10);
+            }}
+            onChange={(value) => {
+              try {
+                const data = yaml.load(value);
+                setApp({ ...app, data });
+              } catch (e) {
+                console.log(e);
+              }
+            }}
           />
-        </Stack>
-      ))}
-      <Stack style={{ justifyContent: "center" }}>
-        <AddProcessorDivider
-          showProcessorSelector={true}
-          setProcessorBackend={(apiBackend) => {
-            const newProcessors = [...processors];
-            newProcessors.push({
-              id: `_inputs${newProcessors.length + 1}`,
-              api_backend: apiBackend,
-              processor_slug: apiBackend?.slug,
-              provider_slug: apiBackend?.api_provider?.slug,
-              endpoint: null,
-              input: null,
-              config: null,
-            });
-            setProcessors(newProcessors);
-            setActiveStep(newProcessors.length + 1);
-          }}
-        />
-      </Stack>
-      <Stack>
-        <AppOutputEditor
-          index={processors.length}
-          activeStep={activeStep}
-          setActiveStep={setActiveStep}
-          outputTemplate={appOutputTemplate}
-          setOutputTemplate={setAppOutputTemplate}
-          outputSchemas={outputSchemas}
-        />
-      </Stack>
+        </Box>
+      ) : (
+        <Box>
+          <Stack>
+            <AppConfigEditor
+              appType={app?.type}
+              activeStep={activeStep}
+              setActiveStep={setActiveStep}
+              config={appConfig}
+              setConfig={setAppConfig}
+              inputFields={appInputFields}
+              setInputFields={setAppInputFields}
+              processors={processors}
+              setProcessors={setProcessors}
+              outputSchemas={outputSchemas}
+            />
+          </Stack>
+          {processors.map((processor, index) => (
+            <Stack style={{ justifyContent: "center" }} key={index}>
+              <AddProcessorDivider showProcessorSelector={false} />
+              <ProcessorEditor
+                appId={app.uuid}
+                index={index}
+                processors={processors}
+                setProcessors={setProcessors}
+                activeStep={activeStep}
+                setActiveStep={setActiveStep}
+                outputSchemas={outputSchemas}
+                isTool={app.app_type_slug === "agent"}
+              />
+            </Stack>
+          ))}
+          <Stack style={{ justifyContent: "center" }}>
+            <AddProcessorDivider
+              showProcessorSelector={true}
+              setProcessorBackend={(apiBackend) => {
+                const newProcessors = [...processors];
+                newProcessors.push({
+                  id: `_inputs${newProcessors.length + 1}`,
+                  api_backend: apiBackend,
+                  processor_slug: apiBackend?.slug,
+                  provider_slug: apiBackend?.api_provider?.slug,
+                  endpoint: null,
+                  input: null,
+                  config: null,
+                });
+                setProcessors(newProcessors);
+                setActiveStep(newProcessors.length + 1);
+              }}
+            />
+          </Stack>
+          <Stack>
+            <AppOutputEditor
+              index={processors.length}
+              activeStep={activeStep}
+              setActiveStep={setActiveStep}
+              outputTemplate={appOutputTemplate}
+              setOutputTemplate={setAppOutputTemplate}
+              outputSchemas={outputSchemas}
+            />
+          </Stack>
+        </Box>
+      )}
       <Stack
         direction="row"
         gap={1}
         sx={{
-          flexDirection: "row-reverse",
           maxWidth: "900px",
           margin: "auto",
+          justifyContent: "space-between",
         }}
       >
+        <Tooltip title="Toggle between UI and YAML editors">
+          <ToggleButtonGroup
+            variant="outlined"
+            color="primary"
+            value={editorType}
+            exclusive
+            sx={{
+              marginBottom: "50px",
+              padding: "14px 0",
+            }}
+            onChange={(e, newEditorType) => {
+              setEditorType(newEditorType);
+            }}
+          >
+            <ToggleButton value="ui">UI</ToggleButton>
+            <ToggleButton value="yaml">YAML</ToggleButton>
+          </ToggleButtonGroup>
+        </Tooltip>
         <AppSaveButtons saveApp={saveApp} />
       </Stack>
     </Box>
