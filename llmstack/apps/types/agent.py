dc0945817c6df8a1114e807245459435ef0e6b19
Ajay Chintala <nitk.ajay@gmail.com>
2023-10-09 15:29:46 -0700

    Fix typo

--- llmstack/apps/types/agent.py
+++ llmstack/apps/types/agent.py
@@ -7,7 +7,7 @@ from llmstack.apps.types.app_type_interface import BaseSchema
 
 class AgentModel(str, Enum):
     GPT_3_5 = 'gpt-3.5-turbo'
-    GPT_3_5_16K = 'gpt-3.6-turbo-16k'
+    GPT_3_5_16K = 'gpt-3.5-turbo-16k'
     GPT_4 = 'gpt-4'
     GPT_4_32K = 'gpt-4-32k'
 
