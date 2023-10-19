6e8a196e0ab3cf9ee4048f1e3e15b718550ff79b
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-10-19 01:52:16 -0700

    Add admin models

---
+++ llmstack/jobs/admin.py
@@ -0,0 +1,101 @@
+from django.conf import settings
+from django.contrib import admin
+
+from .models import ScheduledJob, RepeatableJob, CronJob
+
+QUEUES = [(key, key) for key in settings.RQ_QUEUES.keys()]
+
+class QueueMixin(object):
+    actions = ['delete_model']
+    def get_actions(self, request):
+        actions = super(QueueMixin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+    
+    def get_form(self, request, obj=None, **kwargs):
+        queue_field = self.model._meta.get_field('queue')
+        queue_field.choices = QUEUES
+        return super(QueueMixin, self).get_form(request, obj, **kwargs)
+    
+    def delete_model(self, request, obj):
+        if hasattr(obj, 'all'):
+            for o in obj.all():
+                o.delete()
+        else:
+            obj.delete()
+    
+    delete_model.short_description = "Delete selected %(verbose_name_plural)s"
+
+class ScheduledJobAdmin(QueueMixin, admin.ModelAdmin):
+    list_display = (
+        'name', 'job_id', 'is_scheduled', 'scheduled_time', 'enabled')
+    list_filter = ('enabled', )
+    list_editable = ('enabled', )
+    readonly_fields = ('job_id', )
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'callable', 'enabled', ),
+        }),
+        ('RQ Settings', {
+            'fields': ('queue', 'job_id', ),
+        }),
+        ('Scheduling', {
+            'fields': (
+                'scheduled_time',
+                'timeout',
+                'result_ttl'
+            ),
+        }),
+    )
+
+class RepeatableJobAdmin(QueueMixin, admin.ModelAdmin):
+    list_display = (
+        'name', 'job_id', 'is_scheduled', 'scheduled_time', 'interval_display',
+        'enabled')
+    list_filter = ('enabled', )
+    list_editable = ('enabled', )
+    readonly_fields = ('job_id', )
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'callable', 'enabled', ),
+        }),
+        ('RQ Settings', {
+            'fields': ('queue', 'job_id', ),
+        }),
+        ('Scheduling', {
+            'fields': (
+                'scheduled_time',
+                ('interval', 'interval_unit', ),
+                'repeat',
+                'timeout',
+                'result_ttl'
+            ),
+        }),
+    )
+    
+class CronJobAdmin(QueueMixin, admin.ModelAdmin):
+    list_display = (
+        'name', 'job_id', 'is_scheduled', 'cron_string', 'enabled')
+    list_filter = ('enabled', )
+    list_editable = ('enabled', )
+
+    readonly_fields = ('job_id', )
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'callable', 'enabled', ),
+        }),
+        ('RQ Settings', {
+            'fields': ('queue', 'job_id', ),
+        }),
+        ('Scheduling', {
+            'fields': (
+                'cron_string',
+                'repeat',
+                'timeout',
+            ),
+        }),
+    )
+    
+admin.site.register(ScheduledJob, ScheduledJobAdmin)
+admin.site.register(RepeatableJob, RepeatableJobAdmin)
+admin.site.register(CronJob, CronJobAdmin)
--- llmstack/jobs/apps.py
+++ llmstack/jobs/apps.py
@@ -1,6 +1,38 @@
 from django.apps import AppConfig
+from django.db.models.functions import Now
 
 
 class JobsConfig(AppConfig):
     name = 'llmstack.jobs'
     label = 'jobs'
+    
+    def ready(self):
+        try:
+            self.reschedule_cron_jobs()
+            self.reschedule_repeatable_jobs()
+            self.reschedule_scheduled_jobs()
+        except:
+            # Django isn't ready yet, example a management command is being
+            # executed
+            pass
+    
+    def reschedule_cron_jobs(self):
+        from .models import CronJob
+        jobs = CronJob.objects.filter(enabled=True)
+        self.reschedule_jobs(jobs)
+    
+    def reschedule_repeatable_jobs(self):
+        from .models import RepeatableJob
+        jobs = RepeatableJob.objects.filter(enabled=True)
+        self.reschedule_jobs(jobs)
+    
+    def reschedule_scheduled_jobs(self):
+        from .models import ScheduledJob
+        jobs = ScheduledJob.objects.filter(enabled=True, scheduled_time__lte=Now())
+        self.reschedule_jobs(jobs)
+        
+    def reschedule_jobs(self, jobs):
+        for job in jobs:
+            if job.is_scheduled() is False:
+                print('Scheduling job: {}'.format(job))
+                # job.save()
