55978ca263aaa5e69bf3497f635d05382c570316
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-10-27 10:35:23 -0700

    Update

--- llmstack/client/src/pages/schedule.jsx
+++ llmstack/client/src/pages/schedule.jsx
@@ -13,111 +13,213 @@ import {
   Typography,
   Box,
   Chip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from "@mui/material";
+
 import { axios } from "../data/axios";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import PauseCircleOutlinedIcon from "@mui/icons-material/PauseCircleOutlined";
 import { enqueueSnackbar } from "notistack";
-function Modal({ scheduleType, open, handleCancelCb, handleSubmitCb }) {
-  switch (scheduleType) {
-    case "app":
-      return null;
+
+function ConfirmationModal(props) {
+  const { id, open, onOk, onCancel, title, text } = props;
+  return (
+    <Dialog
+      title={title ? title : "Logged Out"}
+      open={open}
+      onCancel={onCancel}
+    >
+      <DialogTitle id="delete-modal-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{text}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button key="cancel" onClick={onCancel}>
+          Cancel
+        </Button>
+        ,
+        <Button key="submit" variant="contained" type="primary" onClick={onOk}>
+          Confirm
+        </Button>
+        ,
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+function ActionModal({ modalType, open, onCancel, onOk, jobId }) {
+  function deleteAppRunJob() {
+    axios()
+      .delete(`/api/jobs/app_run/${jobId}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        enqueueSnackbar(err.message, { variant: "error" });
+      })
+      .finally(() => {
+        onOk();
+      });
+  }
+  function pauseAppRunJob() {
+    axios()
+      .post(`/api/jobs/app_run/${jobId}/pause`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        enqueueSnackbar(err.message, { variant: "error" });
+      })
+      .finally(() => {
+        onOk();
+      });
+  }
+
+  const resumeAppRunJob = () => {
+    axios()
+      .post(`/api/jobs/app_run/${jobId}/resume`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        enqueueSnackbar(err.message, { variant: "error" });
+      })
+      .finally(() => {
+        onOk();
+      });
+  };
+
+  switch (modalType) {
+    case "delete":
+      // Show a delete confirmation modal
+      return (
+        <ConfirmationModal
+          id="delete-job"
+          open={open}
+          onOk={deleteAppRunJob}
+          onCancel={onCancel}
+          title="Delete Job"
+          text="Are you sure you want to delete this job?"
+        />
+      );
+    case "pause":
+      return (
+        <ConfirmationModal
+          id="pause-job"
+          open={open}
+          onOk={pauseAppRunJob}
+          onCancel={onCancel}
+          title="Pause Job"
+          text="Are you sure you want to pause this job?"
+        />
+      );
+    case "resume":
+      return (
+        <ConfirmationModal
+          id="resume-job"
+          open={open}
+          onOk={onOk}
+          onCancel={onCancel}
+          title="Resume Job"
+          text="Are you sure you want to resume this job?"
+        />
+      );
     default:
       return null;
   }
 }
 
-const AppRunJobsColumn = [
-  {
-    title: "Name",
-    key: "name",
-  },
-  {
-    title: "Type",
-    key: "model",
-    render: (record, row) => {
-      switch (record) {
-        case "ScheduledJob":
-          return <Chip label="Scheduled Job" color="primary" />;
-        case "RepeatableJob":
-          return <Chip label="Repeatable Job" color="primary" />;
-        case "CronJob":
-          return <Chip label="Cron Job" color="primary" />;
-        default:
-          return null;
-      }
-    },
-  },
-  {
-    title: "Input",
-    key: "input",
-    render: (record, row) => {
-      return <Button>View</Button>;
-    },
-  },
-  {
-    title: "Status",
-    key: "enabled",
-    render: (record, row) => {
-      return row ? (
-        <Chip label="Enabled" color="success" />
-      ) : (
-        <Chip label="Disabled" color="error" />
-      );
-    },
-  },
-  {
-    title: "Last Run",
-    key: "last_run",
-  },
-  {
-    title: "Action",
-    key: "operation",
-    render: (record, row) => {
-      console.log(row);
-
-      return (
-        <Box>
-          <IconButton
-            onClick={() => {
-              axios()
-                .post(`/api/jobs/app_run/${row.uuid}/pause`)
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  enqueueSnackbar(err.message, { variant: "error" });
-                });
-            }}
-            color="primary"
-          >
-            <PauseCircleOutlinedIcon />
-          </IconButton>
-          <IconButton
-            onClick={() => {
-              axios()
-                .delete(`/api/jobs/app_run/${row.uuid}`)
-                .then((res) => {
-                  console.log(res);
-                })
-                .catch((err) => {
-                  enqueueSnackbar(err.message, { variant: "error" });
-                });
-            }}
-            color="primary"
-          >
-            <DeleteOutlineOutlinedIcon />
-          </IconButton>
-        </Box>
-      );
-    },
-  },
-];
 export default function Schedule() {
   const [pageNumber, setPageNumber] = useState(1);
   const [scheduledAppRuns, setScheduledAppRuns] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalType, setModalType] = useState(null);
+  const [jobId, setJobId] = useState(null);
+
+  const modalCancelCb = () => {
+    setModalType(null);
+    setJobId(null);
+    setModalOpen(false);
+  };
+
+  const AppRunJobsColumn = [
+    {
+      title: "Name",
+      key: "name",
+    },
+    {
+      title: "Type",
+      key: "model",
+      render: (record, row) => {
+        switch (record) {
+          case "ScheduledJob":
+            return <Chip label="Scheduled Job" color="primary" />;
+          case "RepeatableJob":
+            return <Chip label="Repeatable Job" color="primary" />;
+          case "CronJob":
+            return <Chip label="Cron Job" color="primary" />;
+          default:
+            return null;
+        }
+      },
+    },
+    {
+      title: "Input",
+      key: "input",
+      render: (record, row) => {
+        return <Button>View</Button>;
+      },
+    },
+    {
+      title: "Status",
+      key: "enabled",
+      render: (record, row) => {
+        return row ? (
+          <Chip label="Enabled" color="success" />
+        ) : (
+          <Chip label="Disabled" color="error" />
+        );
+      },
+    },
+    {
+      title: "Last Run",
+      key: "last_run",
+    },
+    {
+      title: "Action",
+      key: "operation",
+      render: (record, row) => {
+        return (
+          <Box>
+            <IconButton
+              onClick={() => {
+                setJobId(row.uuid);
+                setModalType("pause");
+                setModalOpen(true);
+              }}
+              color="primary"
+            >
+              <PauseCircleOutlinedIcon />
+            </IconButton>
+            <IconButton
+              onClick={() => {
+                setJobId(row.uuid);
+                setModalType("delete");
+                setModalOpen(true);
+              }}
+              color="primary"
+            >
+              <DeleteOutlineOutlinedIcon />
+            </IconButton>
+          </Box>
+        );
+      },
+    },
+  ];
 
   useEffect(() => {
     axios()
@@ -126,6 +228,7 @@ export default function Schedule() {
         setScheduledAppRuns(res.data);
       });
   }, []);
+
   return (
     <div id="schedule-page" style={{ marginBottom: "120px" }}>
       <Grid style={{ padding: "10px", width: "100%" }}>
@@ -170,25 +273,26 @@ export default function Schedule() {
               })}
             </TableBody>
           </Table>
-          {/* <Pagination
-            variant="outlined"
-            shape="rounded"
-            page={pageNumber}
-            onChange={(event, value) => {
-              setPageNumber(value);
-            }}
-            sx={{ marginTop: 2, float: "right" }}
-          /> */}
+          {scheduledAppRuns.length > 0 && (
+            <Pagination
+              variant="outlined"
+              shape="rounded"
+              page={pageNumber}
+              onChange={(event, value) => {
+                setPageNumber(value);
+              }}
+              sx={{ marginTop: 2, float: "right" }}
+            />
+          )}
         </Grid>
       </Grid>
       {modalOpen && (
-        <Modal
+        <ActionModal
           open={modalOpen}
-          scheduleType={modalType}
-          handleCancelCb={() => setModalOpen(false)}
-          handleSubmitCb={() => {
-            setModalOpen(false);
-          }}
+          modalType={modalType}
+          onCancel={modalCancelCb}
+          jobId={jobId}
+          onOk={modalCancelCb}
         />
       )}
     </div>
