1f89c70f1cf4c07128bd81601ea3a2ed4768f135
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-11-09 14:35:26 -0800

    Add basic and bearer authentication

---
+++ llmstack/connections/handlers/basic_authentication.py
@@ -0,0 +1,27 @@
+from llmstack.common.blocks.base.schema import BaseSchema
+from llmstack.connections.types import ConnectionType, ConnectionTypeInterface
+
+class BasicAuthenticationConfiguration(BaseSchema):
+    username: str
+    password: str
+    
+class BasicAuthenticationBasedAPILogin(ConnectionTypeInterface[BasicAuthenticationConfiguration]):
+    @staticmethod
+    def name() -> str:
+        return 'Basic Authentication'
+
+    @staticmethod
+    def provider_slug() -> str:
+        return 'promptly'
+
+    @staticmethod
+    def slug() -> str:
+        return 'username_password_api'
+
+    @staticmethod
+    def description() -> str:
+        return 'Username Password based API Login'
+    
+    @staticmethod
+    def connection_type() -> ConnectionType:
+        return ConnectionType.CREDENTIALS
