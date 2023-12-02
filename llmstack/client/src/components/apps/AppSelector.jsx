3e9cd137cb9886ae3d6c68fffb6aa6fde442641a
Ajay Chintala <nitk.ajay@gmail.com>
2023-12-01 16:12:56 -0800

    Update AppSelector component

--- llmstack/client/src/components/apps/AppSelector.jsx
+++ llmstack/client/src/components/apps/AppSelector.jsx
@@ -4,13 +4,15 @@ export function AppSelector(props) {
   return (
     <div>
       <FormControl fullWidth>
-        <InputLabel id="app-select-label">Select an application</InputLabel>
+        <InputLabel id="app-select-label">Select an App</InputLabel>
         <Select
           labelId="app-select-label"
           id="app-select"
-          value={props.value}
+          value={props.value || ""}
           label="Select an application"
           onChange={(event) => props.onChange(event.target.value)}
+          variant="filled"
+          sx={{ lineHeight: "0.5em" }}
         >
           {props.apps.map((app) => (
             <MenuItem key={app.published_uuid} value={app.published_uuid}>
