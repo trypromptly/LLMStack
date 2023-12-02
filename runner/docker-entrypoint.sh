1a9c96a5e1e8367c08ac35d89734866a63d12718
Ajay Chintala <nitk.ajay@gmail.com>
2023-12-02 00:19:26 -0800

    Reuse runner redis for rqworker

--- runner/docker-entrypoint.sh
+++ runner/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Start redis server if RUNNER_REDIS_HOST is not set or is set to localhost
 if [ -z "$RUNNER_REDIS_HOST" ] || [ "$RUNNER_REDIS_HOST" = "localhost" ] || [ "$RUNNER_REDIS_HOST" = "127.0.0.1" ]; then
     echo "Starting redis server"
-    redis-server --daemonize yes
+    redis-server --daemonize yes --protected-mode no
 fi
 
 # Start llmstack-runner
