9fb1eac1f51078d6c5e72c98ad139a40fb0b9725
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-12-08 16:27:45 -0800

    Add api key as connection

---
+++ llmstack/connections/handlers/apikey_authentication.py
@@ -0,0 +1,28 @@
+from pydantic import Field
+from llmstack.common.blocks.base.schema import BaseSchema
+from llmstack.connections.types import ConnectionTypeInterface
+from llmstack.connections.models import ConnectionType
+
+class APIKeyAuthenticationConfiguration(BaseSchema):
+    api_key: str = Field(widget='password', description='Paste your API Key here')
+
+class APIKeyAuthenticationBasedAPILogin(ConnectionTypeInterface[APIKeyAuthenticationConfiguration]):
+    @staticmethod
+    def name() -> str:
+        return 'API Key Authentication'
+
+    @staticmethod
+    def provider_slug() -> str:
+        return 'promptly'
+
+    @staticmethod
+    def slug() -> str:
+        return 'api_key_authentication'
+
+    @staticmethod
+    def description() -> str:
+        return 'API Key based API authentication'
+    
+    @staticmethod
+    def type() -> ConnectionType:
+        return ConnectionType.CREDENTIALS
