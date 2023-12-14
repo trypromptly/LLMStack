93d8ad9bf623c375d3cf5db10cdd1523e9cca8e8
Ajay Chintala <nitk.ajay@gmail.com>
2023-12-13 16:53:53 -0800

    Add jobserializer

--- llmstack/jobs/serializers.py
+++ llmstack/jobs/serializers.py
@@ -1,6 +1,7 @@
+import orjson as json
 from rest_framework import serializers
 
-from llmstack.jobs.models import TaskRunLog
+from llmstack.jobs.models import CronJob, RepeatableJob, ScheduledJob, TaskRunLog
 
 
 class TaskRunLogSerializer(serializers.ModelSerializer):
@@ -13,3 +14,36 @@ class TaskRunLogSerializer(serializers.ModelSerializer):
         model = TaskRunLog
         fields = ['uuid', 'task_type', 'task_uuid', 'result',
                   'status', 'errors', 'created_at']
+
+
+BASE_JOB_FIELDS = ['uuid', 'name', 'task_category', 'enabled',
+                   'source_uuid', 'repeat', 'timeout', 'status', 'created_at', 'updated_at']
+
+
+class BaseJobSerializer(serializers.ModelSerializer):
+    source_uuid = serializers.SerializerMethodField()
+
+    def get_source_uuid(self, obj):
+        try:
+            callable_args = json.loads(obj.callable_args)
+            return callable_args[0]
+        except:
+            return None
+
+
+class ScheduledJobSerializer(BaseJobSerializer):
+    class Meta:
+        model = ScheduledJob
+        fields = BASE_JOB_FIELDS
+
+
+class RepeatableJobSerializer(BaseJobSerializer):
+    class Meta:
+        model = RepeatableJob
+        fields = BASE_JOB_FIELDS
+
+
+class CronJobSerializer(BaseJobSerializer):
+    class Meta:
+        model = CronJob
+        fields = BASE_JOB_FIELDS
