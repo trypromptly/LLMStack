3ceb08b52cc14eec78ad71d0c25e1bde8d9ccb41
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-12-15 16:42:51 -0800

    Update crawler to use playwright and connection

--- llmstack/common/utils/crawlers.py
+++ llmstack/common/utils/crawlers.py
@@ -1,20 +1,23 @@
 import json
+import os
+import asyncio
 import logging
 import multiprocessing as mp
+from typing import Optional
 from urllib.parse import urlparse
 
-from asgiref.sync import async_to_sync
-from playwright.async_api import async_playwright
-from scrapy import Selector
+from scrapy import Selector, Spider
+import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.http import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider
 from scrapy.spiders import Rule
 from scrapy.spiders import SitemapSpider
+from twisted.internet.defer import Deferred
 from unstructured.partition.auto import partition_html
 from scrapy.exceptions import CloseSpider
-
-from django.conf import settings
+from scrapy_playwright.handler import ScrapyPlaywrightDownloadHandler, PERSISTENT_CONTEXT_PATH_KEY, BrowserContextWrapper
 
 CRAWLER_SETTINGS = {
     'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -24,7 +27,6 @@ CRAWLER_SETTINGS = {
 
 
 logger = logging.getLogger(__name__)
-
 
 def get_domain(url):
     parsed_url = urlparse(url)
@@ -72,48 +74,104 @@ def run_sitemap_spider_in_process(sitemap_url):
     ).start()
     return result.get()
 
-
-async def run_playwright(url, connection=None):
-    async with async_playwright() as playwright:
-        storage_state = None
-        if connection:
-            storage_state = connection['configuration']['_storage_state']
-            try:
-                storage_state = json.loads(storage_state)
-            except:
-                pass
-        browser = await playwright.chromium.connect(ws_endpoint=settings.PLAYWRIGHT_URL) if hasattr(settings, 'PLAYWRIGHT_URL') and settings.PLAYWRIGHT_URL else await playwright.chromium.launch()
-        if storage_state:
-            context = await browser.new_context(storage_state=storage_state)
-            page = await context.new_page()
+class PromptlyScrapyPlaywrightDownloadHandler(ScrapyPlaywrightDownloadHandler):
+    def __init__(self, settings):
+        super().__init__(settings)
+        PLAYWRIGHT_URL = f'ws://{os.getenv("RUNNER_HOST", "runner")}:{os.getenv("RUNNER_PLAYWRIGHT_PORT", 50053)}'
+        self.browser_cdp_url = PLAYWRIGHT_URL
+        logging.getLogger("scrapy-playwright").setLevel(logging.ERROR)
+    
+    async def _maybe_connect(self) -> None:
+        async with self.browser_launch_lock:
+            if not hasattr(self, "browser"):
+                logger.info("Connecting using : %s", self.browser_cdp_url)
+                self.browser = await self.browser_type.connect(
+                    ws_endpoint=self.browser_cdp_url, **self.browser_cdp_kwargs
+                )
+                
+    async def _create_browser_context(
+        self,
+        name: str,
+        context_kwargs,
+        spider = None,
+    ):
+        """Create a new context, also launching a local browser or connecting
+        to a remote one if necessary.
+        """
+        if hasattr(self, "context_semaphore"):
+            await self.context_semaphore.acquire()
+        context_kwargs = context_kwargs or {}
+        if context_kwargs.get(PERSISTENT_CONTEXT_PATH_KEY):
+            context = await self.browser_type.launch_persistent_context(**context_kwargs)
+            persistent = True
+            remote = False
+        elif self.browser_cdp_url and self.browser_cdp_url.startswith("ws"):
+            await self._maybe_connect()
+            context = await self.browser.new_context(**context_kwargs)
+            persistent = False
+            remote = True
+        elif self.browser_cdp_url:
+            await self._maybe_connect_devtools()
+            context = await self.browser.new_context(**context_kwargs)
+            persistent = False
+            remote = True
         else:
-            page = await browser.new_page()
-            
-        await page.goto(url)
-        await page.wait_for_timeout(5000)
-        html_content = await page.content()
-        await browser.close()
-        return html_content
+            await self._maybe_launch_browser()
+            context = await self.browser.new_context(**context_kwargs)
+            persistent = False
+            remote = False
 
+        context.on(
+            "close", self._make_close_browser_context_callback(name, persistent, remote, spider)
+        )
+        self.stats.inc_value("playwright/context_count")
+        self.stats.inc_value(f"playwright/context_count/persistent/{persistent}")
+        self.stats.inc_value(f"playwright/context_count/remote/{remote}")
+        logger.debug(
+            "Browser context started: '%s' (persistent=%s, remote=%s)",
+            name,
+            persistent,
+            remote,
+            extra={
+                "spider": spider,
+                "context_name": name,
+                "persistent": persistent,
+                "remote": remote,
+            },
+        )
+        if self.default_navigation_timeout is not None:
+            context.set_default_navigation_timeout(self.default_navigation_timeout)
+        self.context_wrappers[name] = BrowserContextWrapper(
+            context=context,
+            semaphore=asyncio.Semaphore(value=self.max_pages_per_context),
+            persistent=persistent,
+        )
+        self._set_max_concurrent_context_count()
+        return self.context_wrappers[name]
+    
+    def download_request(self, request: Request, spider: Spider) -> Deferred:
+        return super().download_request(request, spider)
 
 class URLSpider(CrawlSpider):
     name = 'url_spider'
 
-    def __init__(self, url, output, max_depth=0, allowed_domains=None, allowed_regex=None, denied_regex=None, use_renderer=False, connection=None, *args, **kwargs):
+    def __init__(self, url, output, max_depth=0, allowed_domains=None, allowed_regex=None, denied_regex=None, use_renderer=False, connection=None, playwright_url=None, *args, **kwargs):
         self.start_urls = [url]
         unstructured_trace = logging.getLogger('unstructured.trace')
         unstructured_trace.disabled = True
+        self.logger.setLevel(logging.ERROR)
         if not allowed_domains:
             self.allowed_domains = [get_domain(url)]
         else:
             self.allowed_domains = allowed_domains.split(',')
 
+        self.playwright_url = playwright_url
         self.rules = (
             Rule(
                 LinkExtractor(
                     allow=(allowed_regex or (r'.*')),
                     deny=(denied_regex or (r'.*')),
-                ), callback='parse_document',
+                ), callback='parse_document'
             ),
         )
         self.custom_settings = {
@@ -128,16 +186,55 @@ class URLSpider(CrawlSpider):
         self.max_depth = max_depth
         self.use_renderer = use_renderer
         self.connection = connection
+    
+    @classmethod
+    def update_settings(cls, settings) -> None:
+        super().update_settings(settings)
+        settings.set('LOG_LEVEL', 'ERROR', priority='spider')
+        extensions = settings.getdict('EXTENSIONS')
+        settings.set('EXTENSIONS', {
+            **extensions,
+            'scrapy.extensions.telnet.TelnetConsole': None,
+        }, priority='spider')
+        download_handler = settings.getdict('DOWNLOAD_HANDLERS')
+        settings.set('DOWNLOAD_HANDLERS', {
+            **download_handler,
+            "http": "llmstack.common.utils.crawlers.PromptlyScrapyPlaywrightDownloadHandler",
+            "https": "llmstack.common.utils.crawlers.PromptlyScrapyPlaywrightDownloadHandler",
+        }, priority='spider')
+        settings.set('TWISTED_REACTOR', "twisted.internet.asyncioreactor.AsyncioSelectorReactor", priority='spider')
+        if os.getenv('RUNNER_HOST', None) and os.getenv('RUNNER_PLAYWRIGHT_PORT', None):
+            PLAYWRIGHT_URL = f'ws://{os.getenv("RUNNER_HOST", None)}:{os.getenv("RUNNER_PLAYWRIGHT_PORT", None)}'
+            settings.set('PLAYWRIGHT_CDP_URL', PLAYWRIGHT_URL)
+            
 
-    def get_html_content(self, response):
-        if self.use_renderer:
-            try:
-                html_content = async_to_sync(run_playwright)(response.url, connection=self.connection)
-                return html_content
-            except Exception as e:
-                logging.exception('Error in fetching file with Playwright')
-        return response.body.decode('utf-8')
-
+    def _get_cookies(self, url):
+        cookies = []
+        if self.connection:
+            _storage_state = json.loads(self.connection.get('configuration', {}).get('_storage_state', '{}'))
+            cookie_list = _storage_state.get('cookies', {})
+            url_domain = '.'.join(urlparse(url).netloc.split('.')[-2:])
+            for cookie_entry in cookie_list:
+                if cookie_entry.get('domain', None):
+                    if cookie_entry['domain'].endswith(url_domain):
+                        cookies.append(cookie_entry)
+        return cookies
+    
+    def start_requests(self):
+        if not self.start_urls and hasattr(self, "start_url"):
+            raise AttributeError(
+                "Crawling could not start: 'start_urls' not found "
+                "or empty (but found 'start_url' attribute instead, "
+                "did you miss an 's'?)"
+            )
+        for url in self.start_urls:
+            cookies = self._get_cookies(url)
+            yield Request(url, meta=dict(playwright=True, 
+                                         cookies=cookies, 
+                                         playwright_context_kwargs={
+                                            "storage_state": json.loads(self.connection['configuration']['_storage_state']) if self.connection else None 
+                                        }))
+            
     def get_text_from_html(self, html_response):
         text_data = [
             text.strip().rstrip() for text in html_response.xpath(
@@ -146,57 +243,59 @@ class URLSpider(CrawlSpider):
         ]
         text_data = [text for text in text_data if text != '']
         return '\n'.join(text_data).strip()
-
-    def parse_start_url(self, response):
-        data = {}
-        data['title'] = response.css('title::text').get()
-        data['url'] = response.url
-
-        html_content = self.get_html_content(response)
+    
+    def _parse_response_data(self, response):
+        html_content = response.body.decode('utf-8')
+        page_data = {}
         if html_content:
-            data['raw_text'] = self.get_text_from_html(
+            page_data = {
+                'raw_text': self.get_text_from_html(
                 Selector(text=html_content, type='html'),
-            )
-            data['html_partition'] = '\n\n'.join(
-                [str(el) for el in partition_html(text=html_content)],
-            )
-            data['html_page'] = html_content
-
-            data['hrefs'] = [
-                link for link in Selector(text=html_content, type='html').css(
-                    'a::attr(href)',
-                ).getall()
-            ]
-
+                ),
+                'html_partition': '\n\n'.join(
+                    [str(el) for el in partition_html(text=html_content)],
+                ),
+                'html_page': html_content,
+                'hrefs': [
+                    link for link in Selector(text=html_content, type='html').css(
+                        'a::attr(href)',
+                    ).getall()
+                ],
+                'videos': [
+                    link for link in Selector(text=html_content, type='html').css(
+                        'video::attr(src)',
+                    ).getall()
+                ],
+            }
+        
+        return {
+            'title': response.css('title::text').get(),
+            'url': response.url,
+            **page_data,
+        }
+        
+    def parse_start_url(self, response):
+        data = self._parse_response_data(response)
         # Add more data extraction here as needed
         self.output.append(data)
         # Follow internal links
         if response.meta.get('depth', 0) < self.max_depth:
             internal_links = data['hrefs']
             for link in internal_links:
-                yield response.follow(link, callback=self.parse_document)
+                if not get_domain(link) in self.allowed_domains:
+                    continue
+
+                req = response.follow(link, callback=self.parse_document)
+                cookies = self._get_cookies(link)
+                yield req.replace(meta=dict(playwright=True, 
+                                            playwright_context_kwargs={
+                                                "storage_state": json.loads(self.connection['configuration']['_storage_state']) if self.connection else None 
+                                                },
+                                            cookies=cookies))
 
     def parse_document(self, response):
-        data = {}
-        data['title'] = response.css('title::text').get()
-        data['url'] = response.url
-
-        html_content = self.get_html_content(response)
-        if html_content:
-            data['raw_text'] = self.get_text_from_html(
-                Selector(text=html_content, type='html'),
-            )
-            data['html_partition'] = '\n\n'.join(
-                [str(el) for el in partition_html(text=html_content)],
-            )
-            data['html_page'] = html_content
-
-            data['hrefs'] = [
-                link for link in Selector(text=html_content, type='html').css(
-                    'a::attr(href)',
-                ).getall()
-            ]
-
+        data = self._parse_response_data(response)
+        self.output.append(data)
         # Add more data extraction here as needed
         if response.url not in self.start_urls:
             self.output.append(data)
@@ -207,7 +306,7 @@ class URLSpider(CrawlSpider):
         )
 
 
-def _run_url_spider_process(url, q, max_depth=0, allowed_domains=None, allowed_regex=None, denied_regex=None, use_renderer=False, connection=None):
+def _run_url_spider_process(url, q, max_depth=0, allowed_domains=None, allowed_regex=None, denied_regex=None, use_renderer=False, connection=None, playwright_url=None):
     output = []
     process = CrawlerProcess(CRAWLER_SETTINGS)
     process.crawl(
@@ -219,16 +318,21 @@ def _run_url_spider_process(url, q, max_depth=0, allowed_domains=None, allowed_r
         output=output,
         use_renderer=use_renderer,
         connection=connection,
+        playwright_url=playwright_url,
     )
     process.start()  # The script will block here until the crawling is finished
     q.put(output)
 
 
-def run_url_spider_in_process(url, max_depth=0, allowed_domains=None, allow_regex=None, deny_regex=None, use_renderer=False, connection=None):
+def run_url_spider_in_process(url, max_depth=0, allowed_domains=None, allow_regex=None, deny_regex=None, use_renderer=False, connection=None, playwright_url=None):
     result = mp.Queue()
-    mp.Process(
+    process = mp.Process(
         target=_run_url_spider_process,
         args=(url, result, max_depth, allowed_domains,
-              allow_regex, deny_regex, use_renderer, connection),
-    ).start()
-    return result.get()
+              allow_regex, deny_regex, use_renderer, connection, playwright_url),
+    )
+    process.start()
+    process.join()
+    process.close()
+    
+    return result.get(block=True)
