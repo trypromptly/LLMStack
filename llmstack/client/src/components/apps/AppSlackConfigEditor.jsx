21cea385bd17ff548c53f2e395c6eadc23d576e7
Vignesh Aigal <vignesh.aigal@gmail.com>
2023-12-27 10:14:01 -0800

    Use themed form in Slack Config

--- llmstack/client/src/components/apps/AppSlackConfigEditor.jsx
+++ llmstack/client/src/components/apps/AppSlackConfigEditor.jsx
@@ -1,57 +1,91 @@
-import { Box, Stack, TextField } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { EmbedCodeSnippet } from "./EmbedCodeSnippets";
 import { AppSaveButtons } from "./AppSaveButtons";
+import validator from "@rjsf/validator-ajv8";
+
+import ThemedJsonForm from "../ThemedJsonForm";
+import { createRef } from "react";
+
+const slackConfigSchema = {
+  type: "object",
+  properties: {
+    app_id: {
+      type: "string",
+      title: "App ID",
+      description: "Application ID",
+    },
+    bot_token: {
+      type: "string",
+      title: "Bot Token",
+      description:
+        "Bot token to use for sending messages to Slack. Make sure the Bot has access to app_mentions:read and chat:write scopes. This token is available at Features > OAuth & Permissions in your app page. More details https://api.slack.com/authentication/oauth-v2",
+    },
+    verification_token: {
+      type: "string",
+      title: "Verification Token",
+      description:
+        "Verification token to verify the request from Slack. This token is available at Features > Basic Information in your app page. More details https://api.slack.com/authentication/verifying-requests-from-slack",
+    },
+    signing_secret: {
+      type: "string",
+      title: "Signing Secret",
+      description:
+        "Signing secret to verify the request from Slack. This secret is available at Features > Basic Information in your app page. More details https://api.slack.com/authentication/verifying-requests-from-slack",
+    },
+  },
+};
+
+const slackConfigUISchema = {
+  app_id: {
+    "ui:widget": "text",
+    "ui:emptyValue": "",
+  },
+  bot_token: {
+    "ui:widget": "password",
+    "ui:emptyValue": "",
+  },
+  verification_token: {
+    "ui:widget": "password",
+    "ui:emptyValue": "",
+  },
+  signing_secret: {
+    "ui:widget": "password",
+    "ui:emptyValue": "",
+  },
+};
 
 export function AppSlackConfigEditor(props) {
-  const { app, saveApp, slackConfig, setSlackConfig } = props;
+  const formRef = createRef();
+
+  function slackConfigValidate(formData, errors, uiSchema) {
+    console.log(props.app);
+    console.log("IN VALIDATOR");
+    if ((formData.bot_token || "").length < 5) {
+      errors.bot_token.addError("Bot token is required");
+    }
+    if ((formData.verification_token || "").length < 5) {
+      errors.verification_token.addError("Verification token is required");
+    }
+    if ((formData.signing_secret || "").length < 5) {
+      errors.signing_secret.addError("Signing secret is required");
+    }
+    return errors;
+  }
 
   return (
     <Box>
       <Stack direction="column" gap={2}>
-        <TextField
-          id="app_id"
-          label="App ID"
-          helperText="Application ID"
-          onChange={(e) =>
-            setSlackConfig({ ...slackConfig, app_id: e.target.value })
-          }
-          defaultValue={slackConfig?.app_id || ""}
-          size="small"
+        <ThemedJsonForm
+          schema={slackConfigSchema}
+          uiSchema={slackConfigUISchema}
+          formData={props.slackConfig || {}}
+          onChange={(e) => props.setSlackConfig(e.formData)}
+          validator={validator}
+          disableAdvanced={true}
+          formRef={formRef}
+          customValidate={slackConfigValidate}
         />
-        <TextField
-          id="bot_token"
-          label="Bot Token"
-          helperText="Bot token to use for sending messages to Slack. Make sure the Bot has access to app_mentions:read and chat:write scopes. This token is available at Features > OAuth & Permissions in your app page. More details https://api.slack.com/authentication/oauth-v2"
-          onChange={(e) =>
-            setSlackConfig({ ...slackConfig, bot_token: e.target.value })
-          }
-          defaultValue={slackConfig?.bot_token || ""}
-          size="small"
-        />
-        <TextField
-          id="verification_token"
-          label="Verification Token"
-          helperText="Verification token to verify the request from Slack. This token is available at Features > Basic Information in your app page. More details https://api.slack.com/authentication/verifying-requests-from-slack"
-          onChange={(e) =>
-            setSlackConfig({
-              ...slackConfig,
-              verification_token: e.target.value,
-            })
-          }
-          defaultValue={slackConfig?.verification_token || ""}
-          size="small"
-        />
-        <TextField
-          id="signing_secret"
-          label="Signing Secret"
-          helperText="Signing secret to verify the request from Slack. This secret is available at Features > Basic Information in your app page. More details https://api.slack.com/authentication/verifying-requests-from-slack"
-          onChange={(e) =>
-            setSlackConfig({ ...slackConfig, signing_secret: e.target.value })
-          }
-          defaultValue={slackConfig?.signing_secret || ""}
-          size="small"
-        />
-        <EmbedCodeSnippet app={app} integration="slack" />
+        <EmbedCodeSnippet app={props.app} integration="slack" />
       </Stack>
       <Stack
         direction="row"
@@ -62,7 +96,17 @@ export function AppSlackConfigEditor(props) {
           margin: "auto",
         }}
       >
-        <AppSaveButtons saveApp={saveApp} />
+        <AppSaveButtons
+          saveApp={() => {
+            return new Promise((resolve, reject) => {
+              if (formRef.current.validateForm() === false) {
+                resolve();
+              } else {
+                props.saveApp().then(resolve).catch(reject);
+              }
+            });
+          }}
+        />
       </Stack>
     </Box>
   );
