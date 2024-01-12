977c1bab4c77b7879e8d01a77c0fc42084e5b47b
Ajay Chintala <nitk.ajay@gmail.com>
2024-01-11 17:16:53 -0800

    Add display name to datasource entry in admin

--- llmstack/datasources/admin.py
+++ llmstack/datasources/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 
-from .models import DataSource
-from .models import DataSourceEntry
-from .models import DataSourceType
+from .models import DataSource, DataSourceEntry, DataSourceType
 
 
 class DataSourceEntryAdmin(admin.ModelAdmin):
     search_fields = ['uuid', 'name', 'datasource__name']
-    
+    list_display = ['name']
+
 
 class DataSourceAdmin(admin.ModelAdmin):
     search_fields = ['uuid', 'owner__email']
