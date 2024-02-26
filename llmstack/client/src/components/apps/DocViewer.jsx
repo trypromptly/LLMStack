1ffb11d07909990da2883fa3d1cf5c25af10bcdf
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-02-26 00:26:10 -0800

    Replace pdf viewer

--- llmstack/client/src/components/apps/DocViewer.jsx
+++ llmstack/client/src/components/apps/DocViewer.jsx
@@ -1,125 +1,61 @@
-import { pdfjs } from "react-pdf";
-import { Document, Page } from "react-pdf";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
 
-import React, { useMemo, useState, memo } from "react";
-import {
-  CircularProgress,
-  Box,
-  TextField,
-  IconButton,
-  Stack,
-} from "@mui/material";
-import ZoomInIcon from "@mui/icons-material/ZoomIn";
-import ZoomOutIcon from "@mui/icons-material/ZoomOut";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { zoomPlugin } from "@react-pdf-viewer/zoom";
+import "@react-pdf-viewer/zoom/lib/styles/index.css";
+import { searchPlugin } from "@react-pdf-viewer/search";
+import "@react-pdf-viewer/search/lib/styles/index.css";
+import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
+import "@react-pdf-viewer/page-navigation/lib/styles/index.css";
+import { fullScreenPlugin } from "@react-pdf-viewer/full-screen";
+import "@react-pdf-viewer/full-screen/lib/styles/index.css";
+import { toolbarPlugin } from "@react-pdf-viewer/toolbar";
+import "@react-pdf-viewer/toolbar/lib/styles/index.css";
 
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.js",
-  import.meta.url,
-).toString();
-
-const ZOOM_FACTOR = 1.2; // Adjust this value for finer control over zoom level changes.
+import React, { useMemo, memo } from "react";
+import { Box } from "@mui/material";
 
 export const PDFViewer = memo(
   (props) => {
     const { file, ...sxProps } = props;
-    const memoizedFileObject = useMemo(() => ({ url: file }), [file]);
-
-    const [numPages, setNumPages] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [zoom, setZoom] = useState(1); // Setting initial zoom level to 1.
-
-    const nextPage = () => {
-      setCurrentPage((prevPage) => Math.min(prevPage + 1, numPages));
+    const memoizedFileObject = useMemo(() => ({ fileUrl: file }), [file]);
+    const fullScreenPluginInstance = fullScreenPlugin();
+    const zoomPluginInstance = zoomPlugin();
+    const searchPluginInstance = searchPlugin();
+    const pageNavigationPluginInstance = pageNavigationPlugin();
+    const toolbarPluginInstance = toolbarPlugin();
+    const { renderDefaultToolbar, Toolbar } = toolbarPluginInstance;
+    const transform = (slot) => {
+      return {
+        ...slot,
+        Upload: () => <></>,
+        Download: () => <></>,
+        EnterFullScreen: () => <></>,
+        SwitchTheme: () => <></>,
+        Print: () => <></>,
+        ShowProperties: () => <></>,
+        Open: () => <></>,
+        Rotate: () => <></>,
+      };
     };
+    toolbarPluginInstance.fullScreenPluginInstance = fullScreenPluginInstance;
+    toolbarPluginInstance.zoomPluginInstance = zoomPluginInstance;
+    toolbarPluginInstance.searchPluginInstance = searchPluginInstance;
+    toolbarPluginInstance.pageNavigationPluginInstance =
+      pageNavigationPluginInstance;
+    toolbarPluginInstance.propertiesPluginInstance = null;
 
-    const prevPage = () => {
-      setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-    };
-
-    const zoomIn = () => {
-      setZoom((prevZoom) => prevZoom * ZOOM_FACTOR);
-    };
-
-    const zoomOut = () => {
-      setZoom((prevZoom) => prevZoom / ZOOM_FACTOR);
-    };
-
-    console.log("Rendring PDFViewer");
-    return memoizedFileObject?.url ? (
-      <Stack
-        sx={{
-          width: "100%",
-          height: "400px",
-          position: "relative",
-          ...sxProps,
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: "10px",
-          }}
-        >
-          <Stack direction="row" spacing={2}>
-            <IconButton
-              variant="contained"
-              onClick={prevPage}
-              disabled={currentPage === 1}
-            >
-              <ArrowBackIcon />
-            </IconButton>
-            <TextField
-              sx={{ width: "50px" }}
-              variant="outlined"
-              size="small"
-              value={currentPage}
-              onChange={(e) => {
-                const page = parseInt(e.target.value, 10);
-                if (page >= 1 && page <= numPages) {
-                  setCurrentPage(page);
-                }
-              }}
-            />
-            <IconButton
-              variant="contained"
-              onClick={nextPage}
-              disabled={currentPage === numPages}
-            >
-              <ArrowForwardIcon />
-            </IconButton>
-          </Stack>
-          <Stack direction="row" spacing={2}>
-            <IconButton onClick={zoomIn} color="primary" aria-label="zoom in">
-              <ZoomInIcon />
-            </IconButton>
-            <IconButton onClick={zoomOut} color="primary" aria-label="zoom out">
-              <ZoomOutIcon />
-            </IconButton>
-          </Stack>
+    return memoizedFileObject?.fileUrl ? (
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Box sx={{ height: "300px", overflow: "scroll", ...sxProps }}>
+          <Box>
+            <Toolbar>{renderDefaultToolbar(transform)}</Toolbar>
+          </Box>
+          <Viewer
+            fileUrl={memoizedFileObject.fileUrl}
+            plugins={[toolbarPluginInstance]}
+          />
         </Box>
-        <Box sx={{ width: "100%", height: "100%", overflow: "auto" }}>
-          <Document
-            file={memoizedFileObject}
-            loading={<CircularProgress />}
-            onLoadSuccess={({ numPages }) => {
-              setNumPages(numPages);
-            }}
-          >
-            <Page
-              key={`page_${currentPage}`}
-              className={`page_${currentPage}`}
-              pageNumber={currentPage}
-              scale={zoom}
-            />
-          </Document>
-        </Box>
-      </Stack>
+      </Worker>
     ) : null;
   },
   (prev, next) => {
