bfd3d776c792f7ffe44cc9980ce62eeef5dab1b8
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2024-03-27 10:10:25 +0530

    feature(connections): Add filters attribute to the connection_id schema field to dynamically filter connections using connection widget (#202)

--- llmstack/datasources/handlers/databases/sql.py
+++ llmstack/datasources/handlers/databases/sql.py
@@ -72,6 +72,9 @@ class SQLDatabaseSchema(DataSourceSchema):
         widget="connection",
         advanced_parameter=False,
         description="Use your authenticated connection to the database",
+        # Filters is a list of strings, each formed by the combination of the connection attributes 'base_connection_type', 'provider_slug', and 'connection_type_slug', separated by '/'.
+        # The pattern followed is: base_connection_type/provider_slug/connection_type_slug. We may skip provider_slug or connection_type_slug if they are not present in the filter string.
+        filters=[ConnectionType.CREDENTIALS + "/basic_authentication"],
     )
 
 
