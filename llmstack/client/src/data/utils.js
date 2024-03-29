227391c1019d05c0fea8ccaf5e19c4d99a8b0c2a
Ajay Chintala <nitk.ajay@gmail.com>
2024-03-28 17:04:24 -0700

    Add accept and maxSize properties to file input fields in getJSONSchemaFromInputFields function

--- llmstack/client/src/data/utils.js
+++ llmstack/client/src/data/utils.js
@@ -132,6 +132,8 @@ export function getJSONSchemaFromInputFields(inputFields) {
         schema.properties[field.name].format = "data-url";
         schema.properties[field.name].pattern =
           "data:(.*);name=(.*);base64,(.*)";
+        schema.properties[field.name].accept = field.accept || "*";
+        schema.properties[field.name].maxSize = field.maxSize || 2000000;
       }
 
       if (field.type === "voice") {
