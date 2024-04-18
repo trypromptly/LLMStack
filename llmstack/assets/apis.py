f5788222df4aacb889b3c29d531d01f0e34b896c
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-04-19 01:46:23 +0530

    Update file operations processor to work with objrefs (#224)

--- llmstack/assets/apis.py
+++ llmstack/assets/apis.py
@@ -39,7 +39,60 @@ class AssetViewSet(viewsets.ModelViewSet):
 
         return DRFResponse(asset)
 
-    def get_asset_data(self, objref, request_user, request_session=None, include_data=False, include_name=False):
+    def get_by_ref_id(self, request, category, ref_id, include_data=False, include_name=False, include_objref=False):
+        asset = self.get_asset_data_by_ref_id(
+            category,
+            ref_id,
+            request.user,
+            request.session,
+            include_data=include_data,
+            include_name=include_name,
+            include_objref=include_objref,
+        )
+        if asset is None:
+            return DRFResponse(status=404)
+
+        return DRFResponse(asset)
+
+    def _get_asset_model(
+        self,
+        model_cls,
+        asset,
+        request_user,
+        request_session,
+        include_data=False,
+        include_name=False,
+        include_objref=False,
+    ):
+        if asset is None or not model_cls.is_accessible(asset, request_user, request_session):
+            return None
+
+        response = {"url": asset.file.url}
+        if "file_name" in asset.metadata:
+            response["name"] = asset.metadata["file_name"]
+
+        if "mime_type" in asset.metadata:
+            response["type"] = asset.metadata["mime_type"]
+
+        if "file_size" in asset.metadata:
+            response["size"] = asset.metadata["file_size"]
+
+        if include_data:
+            response["data_uri"] = model_cls.get_asset_data_uri(asset, include_name=include_name)
+
+        if include_objref:
+            if model_cls == AppSessionFiles:
+                category = "sessionfiles"
+            elif model_cls == AppDataAssets:
+                category = "appdata"
+
+            response["objref"] = f"objref://{category}/{asset.uuid}"
+
+        return response
+
+    def get_asset_data(
+        self, objref, request_user, request_session=None, include_data=False, include_name=False, include_objref=False
+    ):
         if objref is None or not objref.startswith("objref://"):
             return None
 
@@ -58,25 +111,52 @@ class AssetViewSet(viewsets.ModelViewSet):
 
             asset = model_cls.objects.filter(uuid=asset_uuid.strip()).first()
 
-            if asset is None or not model_cls.is_accessible(asset, request_user, request_session):
-                logger.info(f"Asset not found or not accessible: {objref}")
-                return None
-
-            response = {"url": asset.file.url}
-
-            if "file_name" in asset.metadata:
-                response["name"] = asset.metadata["file_name"]
-
-            if "mime_type" in asset.metadata:
-                response["type"] = asset.metadata["mime_type"]
-
-            if "file_size" in asset.metadata:
-                response["size"] = asset.metadata["file_size"]
-
-            if include_data:
-                response["data_uri"] = model_cls.get_asset_data_uri(asset, include_name=include_name)
+            response = self._get_asset_model(
+                model_cls, asset, request_user, request_session, include_data, include_name, include_objref
+            )
 
             return response
+        except Exception as e:
+            logger.error(f"Error retrieving asset: {e}")
+            db.connections.close_all()
+            return None
+
+    def get_asset_data_by_ref_id(
+        self,
+        category,
+        ref_id,
+        request_user,
+        request_session=None,
+        include_data=False,
+        include_name=False,
+        include_objref=False,
+    ):
+        try:
+            model_cls = None
+            if category == "sessionfiles":
+                model_cls = AppSessionFiles
+            elif category == "appdata":
+                model_cls = AppDataAssets
+
+            if not model_cls:
+                logger.error(f"Invalid category for asset model: {category}")
+                return None
+
+            assets = model_cls.objects.filter(ref_id=ref_id.strip())
+
+            result = []
+            for asset in assets:
+                response = self._get_asset_model(
+                    model_cls, asset, request_user, request_session, include_data, include_name, include_objref
+                )
+                if response:
+                    result.append(response)
+
+            if len(result) == 0:
+                return None
+
+            return {"assets": result}
+
         except Exception as e:
             logger.error(f"Error retrieving asset: {e}")
             db.connections.close_all()
