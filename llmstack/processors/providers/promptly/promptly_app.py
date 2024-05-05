b6202852594fb2bbd1481205828356899fde2d68
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-05-05 02:04:52 -0700

    .

--- llmstack/processors/providers/promptly/promptly_app.py
+++ llmstack/processors/providers/promptly/promptly_app.py
@@ -2,17 +2,15 @@ import base64
 import json
 import logging
 import uuid
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from asgiref.sync import async_to_sync
-from pydantic import Field
+from pydantic import Field, root_validator
 
-from llmstack.apps.models import App, AppData
 from llmstack.apps.schemas import OutputTemplate
 from llmstack.apps.yaml_loader import get_input_model_from_fields
 from llmstack.common.blocks.base.schema import BaseSchema as Schema
 from llmstack.common.utils.liquid import render_template
-from llmstack.play.actor import Actor
 from llmstack.processors.providers.api_processor_interface import ApiProcessorInterface
 
 logger = logging.getLogger(__name__)
@@ -55,8 +53,41 @@ def get_input_ui_schema(input_fields=[]):
     return ui_schema
 
 
-class PromptlyAppInput(Schema):
-    input: Dict = Field(default={}, description="Input")
+class PromptlyApp(Schema):
+    promptly_app: str = Field(
+        default="{}", description="Promptly App Configuration", advanced_parameter=False, widget="promptlyapp_select"
+    )
+    _input: Dict = Field(default={}, description="Input", widget="hidden")
+    _promptly_app_uuid: str = Field(default="", description="Promptly App ID", widget="hidden")
+    _promptly_app_published_uuid: str = Field(default="", description="Promptly Published App ID", widget="hidden")
+    _promptly_app_version = Field(default="", description="Promptly App Version", widget="hidden")
+    _promptl_app_input_fields: List[Dict] = Field(default=[], description="Promptly App Input Fields", widget="hidden")
+    _proptly_app: Dict = Field(default={}, description="Promptly App Configuration", widget="hidden")
+    _input_schema: Dict = Field(default={}, description="Input Schema", widget="hidden")
+    _tool_schema: Dict = Field(default={}, description="Tool Schema", widget="hidden")
+
+    @root_validator
+    def validate_input(cls, values):
+        promptly_app = values.get("promptly_app", "{}")
+        promptly_app_json = json.loads(promptly_app)
+
+        values["_promptly_app_uuid"] = promptly_app_json.get("promptly_app_uuid", "")
+        values["_promptly_app_published_uuid"] = promptly_app_json.get("promptly_app_published_uuid", "")
+        values["_promptly_app_version"] = promptly_app_json.get("promptly_app_version", "")
+        values["_promptly_app_input_fields"] = promptly_app_json.get("promptly_app_input_fields", [])
+        values["_promptly_output_template"] = promptly_app_json.get("promptly_output_template", {})
+        values["_input_schema"] = get_json_schema_from_input_fields(
+            name="PromptlyAppInput", input_fields=values["_promptly_app_input_fields"]
+        )
+        values["_tool_schema"] = get_tool_json_schema_from_input_fields(
+            name="PromptlyAppInput", input_fields=values["_promptly_app_input_fields"]
+        )
+        values["input"] = promptly_app_json.get("input", {})
+        return values
+
+
+class PromptlyAppInput(PromptlyApp):
+    pass
 
 
 class PromptlyAppOutput(Schema):
@@ -65,9 +96,6 @@ class PromptlyAppOutput(Schema):
 
 
 class PromptlyAppConfiguration(Schema):
-    app_published_uuid: str = Field(default="", description="Promptly Published App UUID")
-    app_version: str = Field(default="", description="App Version", widget="hidden")
-    input_schema: str = Field(default="", description="App Input Schema", widget="hidden")
     objref: Optional[bool] = Field(
         default=False,
         title="Output as Object Reference",
@@ -77,39 +105,6 @@ class PromptlyAppConfiguration(Schema):
 
 
 class PromptlyAppProcessor(ApiProcessorInterface[PromptlyAppInput, PromptlyAppOutput, PromptlyAppConfiguration]):
-    def __init__(
-        self,
-        input,
-        config,
-        env,
-        output_stream=None,
-        dependencies=[],
-        all_dependencies=[],
-        metadata={},
-        session_data=None,
-        request=None,
-        id=None,
-        is_tool=False,
-        session_enabled=True,
-    ):
-        Actor.__init__(
-            self,
-            dependencies=dependencies,
-            all_dependencies=all_dependencies,
-        )
-
-        self._config = self._get_configuration_class()(**config)
-        self._input = self._get_input_class()(**{"input": input})
-        self._env = env
-        self._id = id
-        self._output_stream = output_stream
-        self._is_tool = is_tool
-        self._request = request
-        self._metadata = metadata
-        self._session_enabled = session_enabled
-
-        self.process_session_data(session_data if session_enabled else {})
-
     @staticmethod
     def name() -> str:
         return "Promptly App"
@@ -140,92 +135,6 @@ class PromptlyAppProcessor(ApiProcessorInterface[PromptlyAppInput, PromptlyAppOu
     def disable_history(self) -> bool:
         return True
 
-    @classmethod
-    def api_backends(cls, context={}) -> Dict:
-        api_backends = []
-        request_user = context.get("request_user")
-        if request_user:
-            promptly_app_uuids = App.objects.filter(owner=request_user).values_list("uuid", "published_uuid")
-            for app_uuid, published_app_uuid in promptly_app_uuids:
-                promptly_app_data = (
-                    AppData.objects.filter(app_uuid=app_uuid, is_draft=False).order_by("-version").first()
-                )
-                if promptly_app_data and promptly_app_data.data.get("config", {}).get("allowed_as_processor"):
-                    api_backends.append(
-                        {
-                            "id": f"{cls.provider_slug()}/{cls.slug()}/{published_app_uuid}",
-                            "name": promptly_app_data.data.get("name"),
-                            "slug": f"{cls.slug()}/{published_app_uuid}",
-                            "api_provider_slug": cls.provider_slug(),
-                            "description": promptly_app_data.data.get("description"),
-                            "input_schema": get_json_schema_from_input_fields(
-                                name="AppStoreInput", input_fields=promptly_app_data.data.get("input_fields", [])
-                            ),
-                            "output_schema": PromptlyAppOutput.get_schema(),
-                            "config_schema": {
-                                "type": "object",
-                                "properties": {
-                                    "app_published_uuid": {
-                                        "type": "string",
-                                        "title": "App Published UUID",
-                                        "description": "Promptly App",
-                                        "default": f"{published_app_uuid}",
-                                    },
-                                    "app_version": {
-                                        "type": "string",
-                                        "title": "App Version",
-                                        "description": "App Version",
-                                        "default": f"{promptly_app_data.version}",
-                                    },
-                                    "input_schema": {
-                                        "type": "string",
-                                        "title": "App Input Schema",
-                                        "description": "App Input Schema",
-                                        "default": f"""{json.dumps(
-                                        get_tool_json_schema_from_input_fields(name='PromptlyAppInput',input_fields=promptly_app_data.data.get('input_fields', [])))}""",
-                                    },
-                                    "objref": {
-                                        "type": "boolean",
-                                        "title": "Output as Object Reference",
-                                        "description": "Return output as object reference instead of raw text.",
-                                        "default": False,
-                                    },
-                                },
-                                "required": ["app_published_uuid"],
-                            },
-                            "input_ui_schema": get_input_ui_schema(promptly_app_data.data.get("input_fields", [])),
-                            "output_ui_schema": PromptlyAppOutput.get_ui_schema(),
-                            "config_ui_schema": {
-                                "app_published_uuid": {
-                                    "ui:label": "App Published UUID",
-                                    "ui:description": "Promptly App",
-                                    "ui:advanced": False,
-                                    "ui:disabled": True,
-                                },
-                                "app_version": {
-                                    "ui:label": "App Version",
-                                    "ui:description": "App Version",
-                                    "ui:advanced": True,
-                                },
-                                "input_schema": {
-                                    "ui:label": "App Input Schema",
-                                    "ui:description": "App Input Schema",
-                                    "ui:advanced": True,
-                                    "ui:disabled": True,
-                                    "ui:widget": "hidden",
-                                },
-                                "objref": {
-                                    "ui:label": "Output as Object Reference",
-                                    "ui:description": "Return output as object reference instead of raw text.",
-                                    "ui:advanced": True,
-                                },
-                            },
-                            "output_template": cls.get_output_template().dict(),
-                        }
-                    )
-
-        return api_backends
-
     async def process_response_stream(self, response_stream, output_template):
         async for resp in response_stream:
             if resp.get("session") and resp.get("csp") and resp.get("template"):
@@ -246,14 +155,16 @@ class PromptlyAppProcessor(ApiProcessorInterface[PromptlyAppInput, PromptlyAppOu
 
     def process(self) -> dict:
         from llmstack.apps.apis import AppViewSet
+        from llmstack.apps.models import AppData
 
-        app = App.objects.filter(published_uuid=self._config.app_published_uuid).first()
-        app_data = AppData.objects.filter(app_uuid=app.uuid, version=self._config.app_version).first()
-        output_template = app_data.data.get("output_template", {}).get("markdown")
-        self._request.data["input"] = self._input.dict()["input"]
+        app_data = AppData.objects.filter(
+            app_uuid=self._input._promptly_app_uuid, version=self._input._promptly_app_version
+        ).first()
+        output_template = app_data.data.get("output_template").get("markdown")
+        self._request.data["input"] = self._input.input
 
         response_stream, _ = AppViewSet().run_app_internal(
-            str(app.uuid),
+            self._input._promptly_app_uuid,
             self._metadata.get("session_id"),
             str(uuid.uuid4()),
             self._request,
