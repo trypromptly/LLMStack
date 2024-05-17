586dd93d55511b1064e60c05a6736a6d49301de0
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-05-17 15:49:44 +0530

    Fix heygen processor and gemini processor (#237)

--- llmstack/common/utils/liquid.py
+++ llmstack/common/utils/liquid.py
@@ -1,3 +1,4 @@
+import json
 from urllib.parse import quote_plus
 
 from liquid import Environment
@@ -5,7 +6,9 @@ from liquid import Environment
 # Add custom filters
 env = Environment()
 
+
 env.add_filter("urlencode", quote_plus)
+env.add_filter("tojson", json.dumps)
 
 
 def render_template(template, data):
