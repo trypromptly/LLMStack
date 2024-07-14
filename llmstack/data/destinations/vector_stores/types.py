cbf1181572159a5840959d66789ab38d8482a227
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-07-13 23:54:44 -0700

    .

--- llmstack/data/destinations/vector_stores/types.py
+++ llmstack/data/destinations/vector_stores/types.py
@@ -2,15 +2,15 @@ from typing import Annotated, Union
 
 from pydantic import Field
 
-from llmstack.data.destinations.vector_stores.chromadb import (
-    ChromaDBVectorStoreConfiguration,
+from llmstack.data.destinations.vector_stores.legacy_chromadb import (
+    PromptlyLegacyChromaDBVectorStoreConfiguration,
 )
 from llmstack.data.destinations.vector_stores.legacy_weaviate import (
     PromptlyLegacyWeaviateVectorStoreConfiguration,
 )
 
 VectorStore = Annotated[
-    Union[PromptlyLegacyWeaviateVectorStoreConfiguration, ChromaDBVectorStoreConfiguration],
+    Union[PromptlyLegacyWeaviateVectorStoreConfiguration, PromptlyLegacyChromaDBVectorStoreConfiguration],
     Field(discriminator="type"),
 ]
 
@@ -18,7 +18,7 @@ VectorStore = Annotated[
 def get_vector_store_configuration(data):
     if data["type"] == "promptly_legacy_weaviate":
         return PromptlyLegacyWeaviateVectorStoreConfiguration(**data)
-    elif data["type"] == "chromadb":
-        return ChromaDBVectorStoreConfiguration(**data)
+    elif data["type"] == "promptly_legacy_chromadb":
+        return PromptlyLegacyChromaDBVectorStoreConfiguration(**data)
 
     raise NotImplementedError(f"Unknown vector store type: {data['type']}")
