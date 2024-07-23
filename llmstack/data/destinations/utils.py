3002f0e3d2c05ecc06fb91504946a64f9e7495d5
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-07-23 00:53:45 -0700

    Update

--- llmstack/data/destinations/utils.py
+++ llmstack/data/destinations/utils.py
@@ -1,9 +1,11 @@
 def get_destination_cls(slug, provider_slug):
     from llmstack.data.destinations.stores.singlestore import SingleStore
     from llmstack.data.destinations.vector_stores.chromadb import ChromaDB
+    from llmstack.data.destinations.vector_stores.pinecone import Pinecone
+    from llmstack.data.destinations.vector_stores.qdrant import Qdrant
     from llmstack.data.destinations.vector_stores.weaviate import Weaviate
 
-    for cls in [ChromaDB, Weaviate, SingleStore]:
+    for cls in [ChromaDB, Weaviate, SingleStore, Pinecone, Qdrant]:
         if cls.slug() == slug and cls.provider_slug() == provider_slug:
             return cls
     return None
