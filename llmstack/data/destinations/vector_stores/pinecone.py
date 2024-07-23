2ba5a4fdb9b42f6abf2020a7f0d21d81dd914512
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-07-23 00:49:23 -0700

    Update pinecone

--- llmstack/data/destinations/vector_stores/pinecone.py
+++ llmstack/data/destinations/vector_stores/pinecone.py
@@ -10,7 +10,7 @@ class Pinecone(BaseDestination):
     index_name: str = Field(description="Index/Collection name", default="text")
     text_key: str = Field(description="Text key", default="text")
     namespace: Optional[str] = Field(description="Namespace", default=None)
-    deployment_name: Optional[str] = Field(description="Deployment name", default="default")
+    deployment_name: Optional[str] = Field(description="Deployment name", default="*")
 
     _deployment_config: Optional[PineconeProviderConfig] = PrivateAttr()
 
@@ -24,15 +24,37 @@ class Pinecone(BaseDestination):
 
     def initialize_client(self, *args, **kwargs):
         from llama_index.vector_stores.pinecone import PineconeVectorStore
+        from pinecone import Pinecone as _Pinecone
 
         datasource = kwargs.get("datasource")
         self._deployment_config = datasource.profile.get_provider_config(
             model_slug=self.slug(), deployment_key=self.deployment_name, provider_slug=self.provider_slug()
         )
+
+        pinecone_client = _Pinecone(api_key=self._deployment_config.auth.api_key)
+        pinecone_index = pinecone_client.Index(self.index_name)
+
         self._client = PineconeVectorStore(
-            api_key=self._deployment_config.api_key.api_key,
+            pinecone_index=pinecone_index,
+            api_key=self._deployment_config.auth.api_key,
             index_name=self.index_name,
-            environment=self._deployment_config.environment,
             namespace=self.namespace,
             text_key=self.text_key,
         )
+
+    def search(self, query: str, **kwargs):
+        from llama_index.core.vector_stores.types import (
+            VectorStoreQuery,
+            VectorStoreQueryMode,
+        )
+
+        vector_store_query = VectorStoreQuery(
+            query_str=query,
+            mode=(
+                VectorStoreQueryMode.HYBRID if kwargs.get("use_hybrid_search", False) else VectorStoreQueryMode.DEFAULT
+            ),
+            alpha=kwargs.get("alpha", 0.75),
+            hybrid_top_k=kwargs.get("limit", 2),
+        )
+
+        return self._client.query(query=vector_store_query)
