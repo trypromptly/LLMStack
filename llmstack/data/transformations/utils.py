ad519af6b1fceb91a395d35c1effbe4ec49c377d
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-07-24 11:42:30 -0700

    .

--- llmstack/data/transformations/utils.py
+++ llmstack/data/transformations/utils.py
@@ -1,7 +1,10 @@
 def get_transformer_cls(slug, provider_slug):
+    from llmstack.data.transformations.llamindex.embeddings_generator import (
+        EmbeddingsGenerator,
+    )
     from llmstack.data.transformations.llamindex.splitters import SentenceSplitter
 
-    for cls in [SentenceSplitter]:
+    for cls in [SentenceSplitter, EmbeddingsGenerator]:
         if cls.slug() == slug and cls.provider_slug() == provider_slug:
             return cls
 
