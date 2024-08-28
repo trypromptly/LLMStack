4787fd010258e59399078c22f8dde233ad335c6c
ajhai <nitk.ajay@gmail.com>
2024-08-27 19:54:36 -0700

    Header icon for data transformer

--- llmstack/client/src/components/sheets/headerIcons.js
+++ llmstack/client/src/components/sheets/headerIcons.js
@@ -1,11 +1,10 @@
 export const headerIcons = {
-  custom: (p) => {
-    console.log(p);
-    return `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+  custom: (
+    p,
+  ) => `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="2" y="2" width="16" height="16" rx="2" fill="${p.bgColor}"/>
       <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial, sans-serif" font-size="10" fill="${p.fgColor}">AR</text>
-    </svg>`;
-  },
+    </svg>`,
   processor_run: (p) =>
     `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="2" y="2" width="16" height="16" rx="2" fill="${p.bgColor}"/>
@@ -15,5 +14,10 @@ export const headerIcons = {
     `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="2" y="2" width="16" height="16" rx="2" fill="${p.bgColor}"/>
       <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial, sans-serif" font-size="10" fill="${p.fgColor}">AR</text>
+    </svg>`,
+  data_transformer: (p) =>
+    `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect x="2" y="2" width="16" height="16" rx="2" fill="${p.bgColor}"/>
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Arial, sans-serif" font-size="10" fill="${p.fgColor}">DT</text>
     </svg>`,
 };
