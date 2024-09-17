56c5487b0ef1654af95340f572eabf9d33781190
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-09-17 11:16:44 -0700

    Add ClientCredentials grant_type

--- llmstack/connections/handlers/oauth2_authentication.py
+++ llmstack/connections/handlers/oauth2_authentication.py
@@ -18,7 +18,7 @@ class HeaderKeyValueType(BaseModel):
 
 
 class RefreshToken(BaseModel):
-    grant_type: Literal["refresh_token"] = "refresh_token"
+    grant_type: Literal["refresh_token"] = Field(default="refresh_token", json_schema_extra={"widget": "hidden"})
     refresh_token: str = Field(json_schema_extra={"widget": "textarea"}, description="Refresh Token", default="")
     headers: Optional[List[HeaderKeyValueType]] = Field(
         default=[HeaderKeyValueType(key="Content-Type", value="application/x-www-form-urlencoded")],
@@ -35,14 +35,24 @@ class RefreshToken(BaseModel):
 
 
 class AuthorizationCode(BaseModel):
-    grant_type: Literal["authorization_code"] = "authorization_code"
+    grant_type: Literal["authorization_code"] = Field(
+        default="authorization_code", json_schema_extra={"widget": "hidden"}
+    )
     code: str = Field(json_schema_extra={"widget": "textarea"}, description="Authorization Code")
     redirect_uri: str = Field(json_schema_extra={"widget": "textarea"}, description="Redirect URI")
 
 
+class ClientCredentials(BaseModel):
+    grant_type: Literal["client_credentials"] = Field(
+        default="client_credentials", json_schema_extra={"widget": "hidden"}
+    )
+
+
 class OAuth2AuthenticationConfiguration(BaseSchema):
     token_url: str
-    grant_type: Union[RefreshToken] = Field(default=RefreshToken(), description="Grant Type")
+    grant_type: Union[RefreshToken, AuthorizationCode, ClientCredentials] = Field(
+        default=RefreshToken(), description="Grant Type"
+    )
     client_id: Optional[str] = Field(default=None, description="Client ID")
     client_secret: Optional[str] = Field(default=None, description="Client Secret")
 
@@ -87,6 +97,8 @@ class OAuth2AuthenticationBasedAPILogin(ConnectionTypeInterface[OAuth2Authentica
             code = config.get("grant_type", {}).get("code")
             redirect_uri = config.get("grant_type", {}).get("redirect_uri")
             config = cls.authorization_code(code, redirect_uri, config)
+        elif config.get("grant_type", {}).get("grant_type") == "client_credentials":
+            config = cls.client_credentials(config)
         return OAuth2AuthenticationConfiguration.model_validate(config)
 
     @classmethod
@@ -144,6 +156,28 @@ class OAuth2AuthenticationBasedAPILogin(ConnectionTypeInterface[OAuth2Authentica
                     "refresh_token": refresh_token,
                     "grant_type": "refresh_token",
                 }
+            return configuration
+        else:
+            return None
+
+    @classmethod
+    def client_credentials(cls, configuration):
+        payload = {
+            "grant_type": "client_credentials",
+        }
+        if configuration.get("client_id"):
+            payload["client_id"] = configuration["client_id"]
+        if configuration.get("client_secret"):
+            payload["client_secret"] = configuration["client_secret"]
+
+        token_url = configuration["token_url"]
+        response = requests.post(token_url, data=payload)
+        if response.status_code == 200:
+            access_token = response.json().get("access_token")
+            expires_in = int(response.json().get("expires_in"))
+            expires_at = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=expires_in)
+            configuration["token"] = access_token
+            configuration["expires_at"] = expires_at.timestamp()
             return configuration
         else:
             return None
