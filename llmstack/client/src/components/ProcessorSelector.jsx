87cdfd0a948e36fdc6fac3ff28778a111479a711
ajhai <nitk.ajay@gmail.com>
2024-09-18 22:34:13 -0700

    Update processor selector styling

--- llmstack/client/src/components/ProcessorSelector.jsx
+++ llmstack/client/src/components/ProcessorSelector.jsx
@@ -1,5 +1,5 @@
-import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import {
+  Autocomplete,
   Box,
   FormControl,
   Grid,
@@ -8,8 +8,12 @@ import {
   ListItemText,
   MenuItem,
   Select,
+  TextField,
   Typography,
+  Stack,
+  Tooltip,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useRecoilValue } from "recoil";
 import { processorsState, providersState, isMobileState } from "../data/atoms";
@@ -17,7 +21,6 @@ import { ProviderIcon } from "./apps/ProviderIcon";
 
 export default function ProcessorSelector({
   onProcessorChange,
-  hideDescription = false,
   defaultProviderSlug = "promptly",
   defaultProcessorSlug = "llm",
 }) {
@@ -115,80 +118,139 @@ export default function ProcessorSelector({
         </Box>
 
         {selectedProvider && (
-          <Box sx={{ minWidth: 150, width: isMobile ? "48%" : "auto" }}>
+          <Box
+            sx={{
+              width: isMobile ? "48%" : "auto",
+              flexGrow: 1,
+            }}
+          >
             <FormControl fullWidth>
-              <InputLabel id="select-processor-label">Processor</InputLabel>
-              <Select
+              <Autocomplete
+                clearIcon={false}
+                id="processor-autocomplete"
+                options={processors.filter(
+                  (x) =>
+                    x.provider.slug === selectedProvider.slug &&
+                    x.name !== "Promptly App",
+                )}
+                getOptionLabel={(option) => option.description}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Processor"
+                    sx={{
+                      "& .MuiInputBase-root": {
+                        textAlign: "center",
+                        padding: "12px 10px !important",
+                        marginLeft: "8px",
+                        fontSize: "0.7rem",
+                        color: "text.secondary",
+                      },
+                      "& .MuiInputBase-root.MuiOutlinedInput-root": {
+                        border: "solid 1px #ccc",
+                      },
+                    }}
+                    InputProps={{
+                      ...params.InputProps,
+                      startAdornment: selectedProcessor && (
+                        <Box
+                          component="span"
+                          sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            ml: 1,
+                          }}
+                        >
+                          <Link
+                            to={`https://docs.trypromptly.com/processors/${selectedProcessor?.provider?.slug}#${selectedProcessor?.slug}`}
+                            target="_blank"
+                            aria-label="Learn more about this Processor"
+                            rel="noreferrer"
+                          >
+                            <Typography variant="body1" color="text.primary">
+                              {selectedProcessor.name}
+                            </Typography>
+                          </Link>
+                        </Box>
+                      ),
+                    }}
+                  />
+                )}
+                onChange={(event, newValue) => {
+                  if (newValue) {
+                    setSelectedProcessor(newValue);
+                    if (onProcessorChange) {
+                      onProcessorChange(newValue);
+                    }
+                  }
+                }}
+                value={selectedProcessor}
+                renderOption={(props, option) => (
+                  <li {...props}>
+                    <Stack spacing={0}>
+                      <Typography
+                        variant="body1"
+                        sx={{ fontWeight: "bold", fontSize: "0.9rem" }}
+                      >
+                        {option.name}
+                      </Typography>
+                      <Tooltip
+                        title={option.description}
+                        disableHoverListener={option.description.length <= 50}
+                      >
+                        <Typography
+                          variant="caption"
+                          color="text.secondary"
+                          sx={{
+                            textOverflow: "ellipsis",
+                            overflow: "hidden",
+                            whiteSpace: "nowrap",
+                            maxWidth: "250px",
+                          }}
+                        >
+                          {option.description}
+                        </Typography>
+                      </Tooltip>
+                    </Stack>
+                  </li>
+                )}
+                filterOptions={(options, { inputValue }) => {
+                  return options.filter(
+                    (option) =>
+                      option.name
+                        .toLowerCase()
+                        .includes(inputValue.toLowerCase()) ||
+                      option.description
+                        .toLowerCase()
+                        .includes(inputValue.toLowerCase()),
+                  );
+                }}
                 sx={{
-                  lineHeight: "32px",
                   "& .MuiSelect-outlined": {
                     textAlign: "center",
                     padding: "6px",
                     marginLeft: "8px",
                     fontSize: "0.9rem",
                   },
+                  "& .MuiInputBase-root": {
+                    paddingRight: "30px !important",
+                    paddingLeft: "12px !important",
+                    fontSize: "0.9rem",
+                    borderRadius: "8px",
+                    marginLeft: "0px !important",
+                    width: "100%",
+                  },
+                  "& .MuiFormLabel-root": {
+                    marginLeft: "-5px",
+                    backgroundColor: "white",
+                    padding: "0px 8px",
+                  },
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    border: "none !important",
+                  },
                 }}
-                onChange={(e) => {
-                  const processor = processors.find(
-                    (processor) =>
-                      processor.provider.slug === selectedProvider.slug &&
-                      processor.slug === e.target.value,
-                  );
-
-                  if (processor) {
-                    setSelectedProcessor(processor);
-
-                    if (onProcessorChange) {
-                      onProcessorChange(processor);
-                    }
-                  }
-                }}
-                value={selectedProcessor ? selectedProcessor.slug : ""}
-                label="Processor"
-              >
-                {processors
-                  .filter(
-                    (x) =>
-                      x.provider.slug === selectedProvider.slug &&
-                      x.name !== "Promptly App",
-                  )
-                  .map((option, index) => (
-                    <MenuItem key={index} value={option.slug}>
-                      {option.name}
-                    </MenuItem>
-                  ))}
-              </Select>
+              />
             </FormControl>
-          </Box>
-        )}
-        {!hideDescription && (
-          <Box>
-            <Typography
-              variant="body1"
-              sx={{
-                display: "flex",
-                border: "solid 1px #ccc",
-                borderRadius: 2,
-                padding: "3px",
-                maxWidth: isMobile ? "100%" : "350px",
-                color: "#666",
-                alignItems: "center",
-                textAlign: "left",
-                gap: 1,
-                backgroundColor: "#f0f7ff",
-              }}
-            >
-              <a
-                href={`https://docs.trypromptly.com/processors/${selectedProcessor?.provider?.slug}`}
-                target="_blank"
-                aria-label="Learn more about this Processor"
-                rel="noreferrer"
-              >
-                <LightbulbIcon color="info" />
-              </a>
-              {selectedProcessor?.description ||
-                "Select a processor from this provider"}
-            </Typography>
           </Box>
         )}
       </Grid>
