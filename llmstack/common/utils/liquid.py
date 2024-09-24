4e4913f203d0b5d5b7aacb3158f662c3b056bde4
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-09-24 14:34:06 -0700

    Add xpath filter to python liquid

--- llmstack/common/utils/liquid.py
+++ llmstack/common/utils/liquid.py
@@ -1,11 +1,14 @@
 import ast
 import json
+import logging
 from urllib.parse import quote_plus
 
+import lxml.etree as ET
 from liquid import Environment
 
 # Add custom filters
 env = Environment()
+logger = logging.getLogger(__name__)
 
 
 def todict(value):
@@ -31,6 +34,29 @@ def escape_unicode(value):
     return value
 
 
+def xpath_filter(xml_string, xpath_expr):
+    # Parse the XML string
+    try:
+        root = ET.fromstring(xml_string)
+    except ET.XMLSyntaxError as e:
+        logger.exception(e)
+
+        return xml_string
+
+    # Apply the XPath expression
+    try:
+        filtered_elements = root.xpath(xpath_expr)
+    except ET.XPathEvalError as e:
+        logger.exception(e)
+        return xml_string
+
+    # Serialize the filtered elements to a string
+    result = ""
+    for element in filtered_elements:
+        result += ET.tostring(element)
+    return result
+
+
 env.add_filter("urlencode", quote_plus)
 env.add_filter("tojson", json.dumps)
 env.add_filter("to_json", json.dumps)
@@ -39,6 +65,7 @@ env.add_filter("to_dict", todict)
 env.add_filter("escape_unicode", escape_unicode)
 env.add_filter("tostring", to_string)
 env.add_filter("to_string", to_string)
+env.add_filter("xpath", xpath_filter)
 
 
 def render_template(template, data):
