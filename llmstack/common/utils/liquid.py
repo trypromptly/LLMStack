f4571f4d59e224ed86ce60d112d9f880634eb004
ajhai <nitk.ajay@gmail.com>
2024-09-26 16:19:56 -0700

    Add html_xpath filter

--- llmstack/common/utils/liquid.py
+++ llmstack/common/utils/liquid.py
@@ -5,6 +5,7 @@ from urllib.parse import quote_plus
 
 import lxml.etree as ET
 from liquid import Environment
+from lxml import html
 
 # Add custom filters
 env = Environment()
@@ -59,6 +60,30 @@ def xpath_filter(xml_string, xpath_expr):
     return list(map(lambda x: ET.tostring(x).decode("ascii"), filtered_elements))
 
 
+def html_xpath_filter(html_string, xpath_expr):
+    # Parse the HTML string
+    try:
+        root = html.fromstring(html_string)
+    except ET.ParserError as e:
+        logger.exception(f"Failed to parse HTML: {e}")
+        return html_string
+
+    # Apply the XPath expression
+    try:
+        filtered_elements = root.xpath(xpath_expr)
+    except ET.XPathEvalError as e:
+        logger.exception(f"Invalid XPath expression: {e}")
+        return html_string
+
+    if len(filtered_elements) == 0:
+        return ""
+
+    if len(filtered_elements) == 1:
+        return html.tostring(filtered_elements[0], encoding="unicode")
+
+    return [html.tostring(elem, encoding="unicode") for elem in filtered_elements]
+
+
 env.add_filter("urlencode", quote_plus)
 env.add_filter("tojson", json.dumps)
 env.add_filter("to_json", json.dumps)
@@ -68,6 +93,7 @@ env.add_filter("escape_unicode", escape_unicode)
 env.add_filter("tostring", to_string)
 env.add_filter("to_string", to_string)
 env.add_filter("xpath", xpath_filter)
+env.add_filter("html_xpath", html_xpath_filter)
 
 
 def render_template(template, data):
