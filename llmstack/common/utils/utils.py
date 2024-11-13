09902da089f3444b41df694193666ec8c0ea8596
Vignesh Aigal <vignesh.aigal@gmail.com>
2024-11-13 10:39:42 -0800

    Change log level

--- llmstack/common/utils/utils.py
+++ llmstack/common/utils/utils.py
@@ -64,7 +64,7 @@ def get_location(ip):
             "longitude": city_loc.location.longitude,
         }
     except Exception as e:
-        logger.exception(e)
+        logger.debug(e)
         return {}
 
 
